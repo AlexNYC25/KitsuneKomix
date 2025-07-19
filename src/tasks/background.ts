72a14678b0a869262cb385e42496d80ef53c566a
Alexis Montes <montes.alexis25@gmail.com>
2025-07-19 02:33:13 -0400

    Updated background comics parser to use in memory lock

--- src/tasks/background.ts
+++ src/tasks/background.ts
@@ -5,47 +5,53 @@ import { parseComicsDirectory } from "../services/comicsParser.ts";
 import logger from "../utilities/logger.ts";
 
 export function startBackgroundComicsParser() {
-  // This function will be called when the background task starts
-  //console.log("Background task started");
   const taskName = "comic_parser";
-  
-  
+  // NOTE: an in-memory lock to prevent concurrent execution of the task, this is on top of the database flag, in the event of db update delays
+  let isRunning = false;
+
   setInterval(() => {
-    const task = getAppTaskQuery(taskName);
-    if (!task) {
-      console.error(`Task "${taskName}" not found in the database.`);
-      return;
-    }
-    if (task.status == "running") {
-      logger.warn("Background Task", `Task "${task.task_name}" is already running. Skipping this interval.`);
+    if (isRunning) {
+      logger.warn(`Background Task "${taskName}" is already running (in-memory lock). Skipping this interval.`);
       return;
     }
 
-    
+    const task = getAppTaskQuery(taskName);
+    if (!task) {
+      logger.error(`Task "${taskName}" not found in the database.`);
+      return;
+    }
+    if (task.running) {
+      logger.warn(`Background Task "${task.task_name}" is already running (DB flag). Skipping this interval.`);
+      return;
+    }
+
+    isRunning = true;
+
     try {
-      logger.info("Background Task", `Starting task "${task.task_name}" with interval ${task.interval} milliseconds`);
-      
+      logger.info(`Background Task "${task.task_name}" is starting with interval ${task.interval} milliseconds`);
+
       const now = new Date().toISOString();
-      updateAppTaskStatus(task.task_name, "running", now, null);
+      updateAppTaskStatus(task.task_name, "running", now, null, true);
 
       parseComicsDirectory()
         .then(() => {
-          logger.info("Background Task", `Task "${task.task_name}" completed successfully.`);
+          logger.info(`Background Task "${task.task_name}" completed successfully.`);
         })
         .catch((error) => {
-          logger.error("Background Task", `Task "${task.task_name}" encountered an error: ${error.message}`);
-
+          logger.error(`Background Task "${task.task_name}" encountered an error: ${error.message}`);
           const completedAt = new Date().toISOString();
-          updateAppTaskStatus(task.task_name, "error", now, completedAt);
+          updateAppTaskStatus(task.task_name, "error", now, completedAt, false);
         })
         .finally(() => {
-          logger.info("Background Task", `Task "${task.task_name}" status updated to completed.`);
-
+          logger.info(`Background Task "${task.task_name}" status updated to completed.`);
           const completedAt = new Date().toISOString();
-          updateAppTaskStatus(task.task_name, "completed", now, completedAt);
+          updateAppTaskStatus(task.task_name, "completed", now, completedAt, false);
+          isRunning = false;
         });
+
     } catch (error) {
-      logger.error("Background Task", `Unexpected error in task "${task.task_name}": ${error}`);
+      logger.error(`Background Task "${task.task_name}" encountered an unexpected error: ${error}`);
+      isRunning = false;
     }
-  }, 5000); // Run every 5 seconds
+  }, 5000);
 }
