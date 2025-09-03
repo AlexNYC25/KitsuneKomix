8d3d9ca67313fc73cd62907acc6abe8b5e2d4dd9
Alexis Montes <montes.alexis25@gmail.com>
2025-09-03 17:45:44 -0400

    Added initilal queries for users

---
+++ src/api/repositories/users.repo.ts
@@ -0,0 +1,50 @@
+import { getDatabase } from "../../config/db/sqliteConnection.ts";
+import { NewUser, UserRow } from "../types/user.type.ts";
+
+const db = getDatabase();
+
+function mapRowToUser(row: Record<string, unknown>): UserRow {
+  return {
+    id: row.id as number,
+    username: row.username as string,
+    email: row.email as string,
+    password_hash: row.password_hash as string,
+    first_name: row.first_name as string | null,
+    last_name: row.last_name as string | null,
+  };
+}
+
+export const createUser = (userData: NewUser): number => {
+  const stmt = db.prepare(`
+    INSERT INTO users (username, email, password_hash, first_name, last_name)
+    VALUES (?, ?, ?, ?, ?)
+  `);
+
+  stmt.run(
+    userData.username,
+    userData.email,
+    userData.passwordHash,
+    userData.firstName ?? null,
+    userData.lastName ?? null,
+  );
+
+  const id = db.lastInsertRowId;
+  return id;
+};
+
+
+export const getUserById = (id: number): UserRow | undefined => {
+  const stmt = db.prepare("SELECT * FROM users WHERE id = ?");
+  const user = stmt.get(id) as Record<string, unknown> | undefined;
+  return user ? mapRowToUser(user) : undefined;
+};
+
+
+
+export const getUserByEmail = (email: string): UserRow | undefined => {
+  const stmt = db.prepare("SELECT * FROM users WHERE email = ?");
+  const row = stmt.get(email) as Record<string, unknown> | undefined;
+  return row ? mapRowToUser(row) : undefined;
+};
+
+
