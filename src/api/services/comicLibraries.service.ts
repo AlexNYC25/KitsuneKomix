2e7cd83332ee32ffe26a9a389d038db05c6514a1
Alexis Montes <montes.alexis25@gmail.com>
2025-09-03 18:59:28 -0400

    Created services for users and comic libraries

---
+++ src/api/services/comicLibraries.service.ts
@@ -0,0 +1,21 @@
+import { LibraryRegistrationInput, NewLibrary } from "../types/comicLibrary.type.ts";
+import { createComicLibrary } from "../repositories/comicLibraries.repo.ts";
+
+export function registerComicLibrary(libraryData: LibraryRegistrationInput): number {
+    const newLibrary: NewLibrary = {
+        name: libraryData.name,
+        description: libraryData.description ?? null,
+        path: libraryData.path,
+        enabled: libraryData.enabled
+    };
+
+    //TODO: Add check to make sure the path is not already registered, and it does not fall within another registered library path
+
+    try {
+        const newLibraryId = createComicLibrary(newLibrary);
+        return newLibraryId;
+    } catch (error) {
+        console.error("Error registering comic library:", error);
+        throw new Error("Internal server error");
+    }
+}
