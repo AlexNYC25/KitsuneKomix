04743d5c111af0171a628313a7ae3a3498c886fe
Alexis Montes <montes.alexis25@gmail.com>
2025-09-03 18:34:11 -0400

    Created types to reflect comic library data

---
+++ src/api/types/comicLibrary.type.ts
@@ -0,0 +1,33 @@
+export type LibraryRow = {
+  id: number;
+  name: string;
+  description: string | null;
+  path: string;
+	enabled: boolean;
+  created_at: string;
+  updated_at: string;
+};
+
+export type LibraryDomain = {
+	id: number;
+	name: string;
+	description?: string;
+	path: string;
+	enabled: boolean;
+	created_at: string;
+	updated_at: string;
+};
+
+export type NewLibrary = {
+	name: string;
+	description?: string | null;
+	path: string;
+	enabled: boolean;
+};
+
+export type LibraryRegistrationInput = {
+	name: string;
+	description?: string | null;
+	path: string;
+	enabled: boolean;
+};
