8157bcad5c5acd98974ecef730124fe93e030817
Alexis Montes <montes.alexis25@gmail.com>
2025-09-07 18:40:10 -0400

    Updated the schema to reflect comic book writers and pencileers and mapping tables to describe relationships between books and roles

--- src/api/db/sqlite/schema.ts
+++ src/api/db/sqlite/schema.ts
@@ -37,6 +37,7 @@ export const comicBooksTable = sqliteTable("comic_books", {
 	id: int().primaryKey({ autoIncrement: true }),
 	library_id: int().notNull().references(() => comicLibrariesTable.id, { onDelete: "cascade" }),
 	file_path: text().notNull(),
+	hash: text().notNull(),
 	title: text(),
 	series: text(),
 	issue_number: text(),
@@ -54,6 +55,38 @@ export const comicSeriesTable = sqliteTable("comic_series", {
 	name: text().notNull(),
 	description: text(),
 	folder_path: text().unique(),
+	created_at: text().notNull().default(sql`CURRENT_TIMESTAMP`),
+	updated_at: text().notNull().default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const comicWritersTable = sqliteTable("comic_writers", {
+	id: int().primaryKey({ autoIncrement: true }),
+	name: text().notNull().unique(),
+	description: text(),
+	created_at: text().notNull().default(sql`CURRENT_TIMESTAMP`),
+	updated_at: text().notNull().default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const comicPencillersTable = sqliteTable("comic_pencillers", {
+	id: int().primaryKey({ autoIncrement: true }),
+	name: text().notNull().unique(),
+	description: text(),
+	created_at: text().notNull().default(sql`CURRENT_TIMESTAMP`),
+	updated_at: text().notNull().default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const comicBookWritersTable = sqliteTable("comic_book_writers", {
+	id: int().primaryKey({ autoIncrement: true }),
+	comic_book_id: int().notNull().references(() => comicBooksTable.id, { onDelete: "cascade" }),
+	comic_writer_id: int().notNull().references(() => comicWritersTable.id, { onDelete: "cascade" }),
+	created_at: text().notNull().default(sql`CURRENT_TIMESTAMP`),
+	updated_at: text().notNull().default(sql`CURRENT_TIMESTAMP`),
+});
+
+export const comicBookPencillersTable = sqliteTable("comic_book_pencillers", {
+	id: int().primaryKey({ autoIncrement: true }),
+	comic_book_id: int().notNull().references(() => comicBooksTable.id, { onDelete: "cascade" }),
+	comic_penciller_id: int().notNull().references(() => comicPencillersTable.id, { onDelete: "cascade" }),
 	created_at: text().notNull().default(sql`CURRENT_TIMESTAMP`),
 	updated_at: text().notNull().default(sql`CURRENT_TIMESTAMP`),
 });
