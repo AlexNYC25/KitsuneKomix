4488d8ffacff0c92a3b0c8a883757391037d5690
Alexis Montes <montes.alexis25@gmail.com>
2025-09-11 17:42:57 -0400

    Added query to get writers linked to comic books

--- src/api/db/sqlite/models/comicWriters.model.ts
+++ src/api/db/sqlite/models/comicWriters.model.ts
@@ -1,6 +1,9 @@
+import { eq } from "drizzle-orm";
+
 import { getClient } from "../client.ts";
 import { comicBookWritersTable, comicWritersTable } from "../schema.ts";
-import { eq } from "drizzle-orm";
+import type { ComicWriter } from "../../../types/index.ts";
+
 
 export const insertComicWriter = async (name: string): Promise<number> => {
   const { db, client } = getClient();
@@ -62,6 +65,34 @@ export const linkWriterToComicBook = async (
       .onConflictDoNothing(); // Avoid duplicate links
   } catch (error) {
     console.error("Error linking writer to comic book:", error);
+    throw error;
+  }
+};
+
+export const getWritersByComicBookId = async (
+  comicBookId: number,
+): Promise<ComicWriter[]> => {
+  const { db, client } = getClient();
+
+  if (!db || !client) {
+    throw new Error("Database is not initialized.");
+  }
+
+  try {
+    const result = await db
+      .select(
+        { comicWriter: comicWritersTable },
+      )
+      .from(comicWritersTable)
+      .innerJoin(
+        comicBookWritersTable,
+        eq(comicWritersTable.id, comicBookWritersTable.comic_writer_id),
+      )
+      .where(eq(comicBookWritersTable.comic_book_id, comicBookId));
+
+    return result.map(({ comicWriter }) => comicWriter);
+  } catch (error) {
+    console.error("Error fetching writers by comic book ID:", error);
     throw error;
   }
 };
