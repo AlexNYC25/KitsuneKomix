0d9e692e73760232ffcdbaa4428b7091f3bedde9
Alexis Montes <montes.alexis25@gmail.com>
2025-09-11 00:36:31 -0400

    fixed admin purget route to handle model call correctly

--- src/api/routes/api.admin.router.ts
+++ src/api/routes/api.admin.router.ts
@@ -4,8 +4,10 @@ import { purgeAllData } from "../db/sqlite/models/admin.model.ts";
 
 const app = new Hono();
 
-app.post("/purge-data", (_c: Context) => {
-  return purgeAllData();
+app.post("/purge-data", async (c: Context) => {
+  await purgeAllData();
+
+  return c.json({ message: "All data purged successfully" }, 200);
 });
 
 export default app;
