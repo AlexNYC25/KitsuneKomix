0d3b93d8acaff63822c25f4e0c699e096e706b03
Alexis Montes <montes.alexis25@gmail.com>
2025-09-12 02:19:04 -0400

    Created a type to be a super version of the standard comic book type with medadata

--- src/api/types/comicBook.type.ts
+++ src/api/types/comicBook.type.ts
@@ -2,6 +2,21 @@ import {
   ComicBook,
   ComicBookWithRelations,
   NewComicBook,
+  ComicWriter,
+  ComicPenciller,
+  ComicInker,
+  ComicLetterer,
+  ComicEditor,
+  ComicColorist,
+  ComicCoverArtist,
+  ComicPublisher,
+  ComicImprint,
+  ComicGenre,
+  ComicCharacter,
+  ComicTeam,
+  ComicLocation,
+  ComicStoryArc,
+  ComicSeriesGroup
 } from "./database.types.ts";
 
 // Re-export database types
@@ -69,4 +84,22 @@ export type ComicBookSearchParams = {
   tags?: string[];
   libraryId?: number;
   read?: boolean;
+};
+
+export type ComicBookWithMetadata = ComicBook & {
+  writers?: ComicWriter[];
+  pencillers?: ComicPenciller[];
+  inkers?: ComicInker[];
+  letterers?: ComicLetterer[];
+  editors?: ComicEditor[];
+  colorists?: ComicColorist[];
+  coverArtists?: ComicCoverArtist[];
+  publishers?: ComicPublisher[];
+  imprints?: ComicImprint[];
+  genres?: ComicGenre[];
+  characters?: ComicCharacter[];
+  teams?: ComicTeam[];
+  locations?: ComicLocation[];
+  storyArcs?: ComicStoryArc[];
+  seriesGroups?: ComicSeriesGroup[];
 };
