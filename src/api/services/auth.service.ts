ba784d45bad118983b7eacdded8b28f5d5c0c44b
Alexis Montes <montes.alexis25@gmail.com>
2025-09-14 23:49:18 -0400

    Added auth service to facilitate the actual check if email is registered and verify passed password is correct

---
+++ src/api/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { getUserByEmail } from "../db/sqlite/models/users.model.ts";
+
+import { verifyPassword } from "../utilities/hash.ts";
+
+export const authenticateUser = async (email: string, password: string) => {
+  const user = await getUserByEmail(email);
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  // Verify the password (you should hash and compare in a real app)
+  if (!(await verifyPassword(user.password_hash, password))) {
+    throw new Error("Invalid password");
+  }
+
+  return { id: user.id, email: user.email, admin: user.admin === 1 };
+}
