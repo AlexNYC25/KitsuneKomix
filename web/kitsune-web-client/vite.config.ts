a81b86ee81000577c05ddafbdd693a26f72d7ace
Alexis Montes <montes.alexis25@gmail.com>
2025-09-22 22:48:17 -0400

    Updated vite config to work better with the docker container

--- web/kitsune-web-client/vite.config.ts
+++ web/kitsune-web-client/vite.config.ts
@@ -4,4 +4,12 @@ import vue from "@vitejs/plugin-vue";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: '0.0.0.0', // Allow external connections (required for Docker)
+    port: 5173,
+    strictPort: true,
+    watch: {
+      usePolling: true, // Required for file watching in Docker containers
+    },
+  },
 });
