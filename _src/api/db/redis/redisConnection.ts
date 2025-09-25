06c2cc3f05147d77ff32298ab7ff28140eb2ddec
Alexis Montes <montes.alexis25@gmail.com>
2025-09-24 22:43:41 -0400

    Deleted old code

--- _src/api/db/redis/redisConnection.ts
+++
@@ -1,8 +0,0 @@
-export const redisConnection = {
-  host: Deno.env.get("REDIS_HOST") ?? "redis",
-  port: Number(Deno.env.get("REDIS_PORT") ?? 6379),
-
-  // Optional extras for stability in containers:
-  maxRetriesPerRequest: null, // recommended for BullMQ
-  enableReadyCheck: true,
-};
