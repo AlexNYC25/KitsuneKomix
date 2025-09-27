2e40e9b1da3b7d3f11ee1bfad765c2a27feedee0
Alexis Montes <montes.alexis25@gmail.com>
2025-09-27 17:50:19 -0400

    Updated the logger to instead of using pretty-pino instead they go right into the output file

--- kitsume-komix-api/logger/loggers.ts
+++ kitsume-komix-api/logger/loggers.ts
@@ -13,39 +13,15 @@ try {
 
 export const apiLogger = pino({
   level: "debug",
-  transport: {
-    target: "pino-pretty",
-    options: {
-      translateTime: "SYS:standard",
-      ignore: "pid,hostname",
-      destination: join(logsDir, "api.log"),
-    },
-  },
-});
+}, pino.destination(join(logsDir, "api.log")));
 
 export const dbLogger = pino({
   level: "debug",
-  transport: {
-    target: "pino-pretty",
-    options: {
-      translateTime: "SYS:standard",
-      ignore: "pid,hostname",
-      destination: join(logsDir, "db.log"),
-    },
-  },
-});
+}, pino.destination(join(logsDir, "db.log")));
 
 export const queueLogger = pino({
   level: "debug",
-  transport: {
-    target: "pino-pretty",
-    options: {
-      translateTime: "SYS:standard",
-      ignore: "pid,hostname",
-      destination: join(logsDir, "queue.log"),
-    },
-  },
-});
+}, pino.destination(join(logsDir, "queue.log")));
 
 // Fallback simple loggers if pino-pretty fails
 export const simpleApiLogger = pino({
