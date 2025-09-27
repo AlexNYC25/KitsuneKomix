858a8d14863e759d0d07bc10e8cd7324e48dffad
Alexis Montes <montes.alexis25@gmail.com>
2025-09-27 17:52:32 -0400

    added db schema files

---
+++ kitsume-komix-api/schemas/comicLibrary.schema.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+export const ComicLibrarySchema = z.object({
+  id: z.number().int().positive().optional(),
+  name: z.string().min(1).max(100),
+  description: z.string().max(255).nullable().optional(),
+  path: z.string().min(1).max(255),
+  enabled: z.boolean(),
+});
