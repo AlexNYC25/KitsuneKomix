04fbb0e5f7de73df8a6c00490dfaf440fc12935b
Alexis Montes <montes.alexis25@gmail.com>
2025-10-01 02:33:52 -0400

    Created auth pinia store with fetch request to get the user's token and info

---
+++ kitsume-komix-client/src/stores/auth.ts
@@ -0,0 +1,48 @@
+import { defineStore } from 'pinia'
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    token: null as string | null,
+		refreshToken: null as string | null,
+    user: null as { id: number; email: string, admin: boolean } | null,
+    loading: false,
+    error: null as string | null,
+  }),
+  getters: {
+    isAuthenticated: (state) => !!state.token
+  },
+  actions: {
+    login({username, password}: {username: string, password: string}) {
+      this.loading = true
+      this.error = null
+      // use the fetch api to call the backend on port 8000
+			fetch('http://localhost:8000/api/auth/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ email: username, password })
+			})
+			.then(response => {
+				if (!response.ok) {
+					throw new Error('Network response was not ok')
+				}
+				return response.json()
+			})
+			.then(data => {
+				this.token = data.accessToken
+				this.refreshToken = data.refreshToken
+				this.user = { id: data.user.id, email: data.user.email, admin: data.user.admin }
+			})
+			.catch(error => {
+				this.error = error.message
+			})
+			.finally(() => {
+				this.loading = false
+			})
+    },
+    logout() {
+      this.token = null
+    }
+  }
+})
