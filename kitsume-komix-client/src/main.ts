1624f75e2c0958f7171e78c53e8a80c98090815c
Alexis Montes <montes.alexis25@gmail.com>
2025-09-30 23:52:42 -0400

    Added vue router + pinia stores + primavue components config to the vue app start up, added router redirection for auth protected routes

--- kitsume-komix-client/src/main.ts
+++ kitsume-komix-client/src/main.ts
@@ -2,10 +2,36 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
+import PrimeVue from 'primevue/config';
+
+import { useAuthStore } from './stores/auth'
+
+import App from './App.vue';
+import Home from './pages/Home.vue';
+import Login from './pages/Login.vue'
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue)
 
+const router = createRouter({
+  history: createMemoryHistory(),
+  routes: [
+    { path: '/', component: Home, meta: { requiresAuth: true } },
+    { path: '/login', component: Login, meta: { requiresAuth: false } },
+  ],
+})
+
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    next('/login')
+  } else {
+    next()
+  }
+})
+
+app.use(router)
 app.mount('#app')
