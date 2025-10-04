fc71f7b8724efb9405f9b3e3a1777c5e6e151cff
Alexis Montes <montes.alexis25@gmail.com>
2025-10-03 21:42:28 -0400

    Updated main app to include the placeholder top bar

--- kitsume-komix-client/src/App.vue
+++ kitsume-komix-client/src/App.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
 import SideBar from './components/SideBar.vue';
+import TopBar from './components/TopBar.vue';
+
 </script>
 
 <template>
-  <header>
-  </header>
 
-  
   <main class="flex h-screen w-screen">
     <SideBar />
-    <div class="flex-1 bg-gray-100 p-4">
-      <RouterView />
+    <div id="top-bar" class="flex-1 bg-gray-100 p-4">
+      <div class="flex flex-col h-full">
+        <TopBar />
+        <RouterView />
+      </div>
+      
     </div>
   </main>
     
