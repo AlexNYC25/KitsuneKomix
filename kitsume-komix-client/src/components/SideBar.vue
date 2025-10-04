a96e3bf2d707c1fd361264a2ce5c5af573a6cd36
Alexis Montes <montes.alexis25@gmail.com>
2025-10-03 22:36:32 -0400

    Updated sidebar to remove dividers and set up dynamic icon lookup

--- kitsume-komix-client/src/components/SideBar.vue
+++ kitsume-komix-client/src/components/SideBar.vue
@@ -25,34 +25,65 @@ const libraries = ref([
 	}
 ]);
 
+const iconLookup: Record<string, string> = {
+  'Latest Comics': 'md-menubook-sharp',
+  'Latest Series': 'io-library-sharp',
+  'All Comics': 'md-menubook-sharp',
+  'All Comic Series': 'io-library-sharp',
+  'All Comic Series Groups': 'hi-solid-library',
+  'All Comic Readlists': 'md-librarybooks-sharp',
+};
+
+// Add icons to the libraries structure dynamically
+libraries.value = libraries.value.map((library) => ({
+  ...library,
+  items: library.items.map((item) => ({
+    ...item,
+    icon: iconLookup[item.label] || '', // Add icon if it exists in the lookup
+  })),
+}));
 </script>
 
 <template>
-  <div id="sidebar" class="bg-gray-800 text-white w-64 h-screen">
+  <div id="sidebar" class="	bg-sky-950 text-white w-64 h-screen">
     <div id="sidebar-header">
-			<Button icon="pi pi-home" class="p-button-text p-button-plain" />
       <h2>Kitsume Komix</h2>
     </div>
 
 		<div id="sidebar-content">
-			<div id="sidebar-home-section" class="sidebar-section">
-				<Button label="Home" variant="text" />
+			<div id="sidebar-home-section" class="sidebar-section mb-2 mt-2">
+				<Button variant="text" class="w-full flex !justify-start items-center">
+					<v-icon name="la-home-solid" class="ml-2"/>
+					<p class="ml-2">
+						Home
+					</p>
+				</Button>
 			</div>
-			<Divider />
+			
 
 			<div id="sidebar-bookmarks-section" >
 				<h3>Bookmarks</h3>
-				<div v-for="bookmark in bookmarks" :key="bookmark.label">
-					<Button :label="bookmark.label" variant="text" class="w-full justify-start" />
+				<div v-for="bookmark in bookmarks" :key="bookmark.label" class="flex items-center">
+				  <Button :label="bookmark.label" variant="text" class="w-full flex !justify-start items-center">
+					<v-icon v-if="iconLookup[bookmark.label]" :name="iconLookup[bookmark.label]" class="ml-2" />
+					<p class="ml-2">{{ bookmark.label }}</p>
+				  </Button>
 				</div>
-				<Divider />
 			</div>
 
 			<div id="sidebar-libraries">
 				<h3>Libraries</h3>
-				<PanelMenu :model="libraries" class="w-full" />
-				<Divider />
+				<PanelMenu :model="libraries" class="">
+					<template #item="{ item }">
+						<div class="card flex items-center">
+							<v-icon v-if="item.icon" :name="item.icon" class="ml-2" />
+							<span>{{ item.label }}</span>
+						</div>
+					</template>
+				</PanelMenu>
 			</div>
+
+			
 		</div>
 
 		<div id="sidebar-footer">
@@ -70,3 +101,6 @@ const libraries = ref([
 		</div>
   </div>
 </template>
+
+<style scoped>
+</style>
