0581ce9fedd7f511b4e9151dd773311aff0e1e6d
Alexis Montes <montes.alexis25@gmail.com>
2025-10-03 21:40:52 -0400

    Created basic topbar component to complimet the sidebar

---
+++ kitsume-komix-client/src/components/TopBar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<header class="flex justify-between items-center mb-4 bg-amber-400">
+		<div id="top-bar-left">
+			<div>
+				<p>Welcome, User!</p>
+			</div>
+		</div>
+		<div id="top-bar-right" class="flex-grow ml-auto max-w-lg">
+			<InputGroup class="bg-blue-100">
+				<InputText placeholder="Keyword" />
+				<InputGroupAddon>
+					<i class="pi pi-search" />
+				</InputGroupAddon>
+			</InputGroup>
+		</div>
+	</header>
+</template>
