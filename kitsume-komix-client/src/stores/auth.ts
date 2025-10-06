6fa480a5551b312a73831ebd61ec72574cb028e5
Alexis Montes <montes.alexis25@gmail.com>
2025-10-06 02:01:32 -0400

    added the refresh action for requesting a new jwt token using the refresh token

--- kitsume-komix-client/src/stores/auth.ts
+++ kitsume-komix-client/src/stores/auth.ts
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: null as string | null,
-		refreshToken: null as string | null,
+	refreshToken: null as string | null,
     user: null as { id: number; email: string, admin: boolean } | null,
     loading: false,
     error: null as string | null,
@@ -41,6 +41,39 @@ export const useAuthStore = defineStore('auth', {
 				this.loading = false
 			})
     },
+	refresh() {
+		if (!this.refreshToken) {
+			this.logout()
+			return
+		}
+		this.loading = true
+		this.error = null
+		fetch('http://localhost:8000/api/auth/refresh-token', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ refreshToken: this.refreshToken })
+		})
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok')
+			}
+			return response.json()
+		})
+		.then(data => {
+			this.token = data.accessToken
+			this.refreshToken = data.refreshToken
+			this.user = { id: data.user.id, email: data.user.email, admin: data.user.admin }
+		})
+		.catch(error => {
+			this.error = error.message
+			this.logout()
+		})
+		.finally(() => {
+			this.loading = false
+		})
+	},
     logout() {
       this.token = null
     }
