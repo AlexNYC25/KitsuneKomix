256b7875cb4c958b510280637a76a1f31d8f7ce3
Alexis Montes <montes.alexis25@gmail.com>
2025-10-05 20:48:04 -0400

    Updated the parent api roiuter to tuse the new openapihono router and import the missing ones

--- kitsume-komix-api/api/routes/api.router.ts
+++ kitsume-komix-api/api/routes/api.router.ts
@@ -1,16 +1,24 @@
-import { Hono } from "hono";
+import { OpenAPIHono } from "@hono/zod-openapi";
 import apiUsersRouter from "./api.users.router.ts";
 import comicLibraryRouter from "./api.comiclibraries.router.ts";
 import adminRouter from "./api.admin.router.ts";
 import comicBookRouter from "./api.comicbooks.router.ts";
 import authRouter from "./api.auth.router.ts";
+import comicSeriesRouter from "./api.comicseries.router.ts";
+import comicPagesRouter from "./api.comicpages.router.ts";
+import collectionsRouter from "./api.collections.router.ts";
+import readlistsRouter from "./api.readlists.router.ts";
 
-const apiRouter = new Hono();
+const apiRouter = new OpenAPIHono();
 
 apiRouter.route("/users", apiUsersRouter);
 apiRouter.route("/comic-libraries", comicLibraryRouter);
 apiRouter.route("/auth", authRouter);
 apiRouter.route("/comic-books", comicBookRouter);
 apiRouter.route("/admin", adminRouter);
+apiRouter.route("/comic-series", comicSeriesRouter);
+apiRouter.route("/comic-pages", comicPagesRouter);
+apiRouter.route("/collections", collectionsRouter);
+apiRouter.route("/readlists", readlistsRouter);
 
 export default apiRouter;
