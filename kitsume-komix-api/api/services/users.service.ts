88405a5c65cf65b4f0f205306f7bc178da9bd04f
Alexis Montes <montes.alexis25@gmail.com>
2025-10-05 20:55:34 -0400

    Updatd user service with function to facilitate assigning a library to a user

--- kitsume-komix-api/api/services/users.service.ts
+++ kitsume-komix-api/api/services/users.service.ts
@@ -1,6 +1,7 @@
 import { hashPassword } from "../../utilities/hash.ts";
 import { UserRegistrationInput } from "../../types/index.ts";
-import { createUser, getUserByEmail } from "../../db/sqlite/models/users.model.ts";
+import { createUser, getUserByEmail, getUserById } from "../../db/sqlite/models/users.model.ts";
+import { assignLibraryToUser, getComicLibraryById } from "../../db/sqlite/models/comicLibraries.model.ts";
 
 export async function createUserService(
   user: UserRegistrationInput,
@@ -33,4 +34,43 @@ export async function createUserService(
   });
 
   return newUserId;
+}
+
+/**
+ * Assign a comic library to a user
+ * @param userId 
+ * @param libraryId 
+ */
+export async function assignLibraryToUserService(
+  userId: number,
+  libraryId: number,
+): Promise<void> {
+  // first we want to validate that the userId and libraryId exist in their respective tables
+  try {
+    const user = await getUserById(userId);
+    if (!user) {
+      throw new Error("User does not exist");
+    }
+  } catch (error) {
+    console.error("Error fetching user by ID:", error);
+    throw new Error("Internal server error");
+  }
+
+  try {
+    const library = await getComicLibraryById(libraryId);
+    if (!library) {
+      throw new Error("Library does not exist");
+    }
+  } catch (error) {
+    console.error("Error fetching library by ID:", error);
+    throw new Error("Internal server error");
+  }
+
+
+  try {
+    await assignLibraryToUser(userId, libraryId);
+  } catch (error) {
+    console.error("Error assigning library to user:", error);
+    throw new Error("Internal server error");
+  }
 }
