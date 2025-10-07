81d1bf63397d75a1a8e044e08ffddf82ae9c45c9
Alexis Montes <montes.alexis25@gmail.com>
2025-10-07 16:43:32 -0400

    Created a schema representing the auth header expected for protected routes

---
+++ kitsume-komix-api/zod/schemas/header.schema.ts
@@ -0,0 +1,8 @@
+import { z } from "@hono/zod-openapi";
+
+export const AuthHeaderSchema = z.object({
+  authorization: z.string().openapi({ 
+    example: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...",
+    description: "Bearer token for authentication"
+  }),
+});
