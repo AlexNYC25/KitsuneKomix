d17730e1a06ce91a5969153a36bfe948835116d7
Alexis Montes <montes.alexis25@gmail.com>
2025-10-10 02:17:34 -0400

    Updated the login form to be part of a card and added the remember me checkbox

--- kitsume-komix-client/src/pages/Login.vue
+++ kitsume-komix-client/src/pages/Login.vue
@@ -13,10 +13,12 @@ import InputText from 'primevue/inputtext';
 import Message from 'primevue/message';
 import Password from 'primevue/password';
 import Button from 'primevue/button';
+import Checkbox from 'primevue/checkbox';
 
 const initialFormData = reactive({
     email: '',
-    password: ''
+    password: '',
+    rememberMe: false
 });
 
 const validateForm = zodResolver(
@@ -40,19 +42,29 @@ const loginFormSubmit = async (event: FormSubmitEvent) => {
 </script>
 
 <template>
-  <div class="login flex-1 bg-gray-100 p-4">
-    <h1>Login</h1>
-    <Form v-slot="$form" :formData="initialFormData" :resolver="validateForm" @submit="loginFormSubmit" class="flex justify-center flex-col gap-4">
+  <div id="login-page" class="flex justify-center items-center h-screen bg-gray-600">
+    <div class="card bg-black shadow-lg p-6 rounded-lg">
+      <div class="flex justify-center mb-4">
+        <h1 class="text-2xl font-bold mb-4 text-white">
+          Kistume Komix Login
+        </h1>
+      </div>
+      <Form v-slot="$form" :formData="initialFormData" :resolver="validateForm" @submit="loginFormSubmit" class="flex justify-center flex-col gap-4">
         <div class="flex flex-col gap-1">
             <InputText name="email" placeholder="Email" />
             <Message v-if="$form.email?.invalid" severity="error" size="small" variant="simple">{{ $form.email.error.message }}</Message>
         </div>
         <div class="flex flex-col gap-1">
-            <Password name="password" placeholder="Password" :feedback="false" />
+            <Password name="password" placeholder="Password" :feedback="false" toggleMask fluid/>
             <Message v-if="$form.password?.invalid" severity="error" size="small" variant="simple">{{ $form.password.error.message }}</Message>
+        </div>
+        <div class="flex align-items-center">
+          <Checkbox v-model="initialFormData.rememberMe" />
+          <label class="ml-2 text-white">Remember Me</label>
         </div>
         <Button type="submit" label="Submit" />
       </Form>
+    </div>
   </div>
 </template>
 
