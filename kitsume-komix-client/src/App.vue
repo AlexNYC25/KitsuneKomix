81af898a37d123c277b5e5759b4ba8e8c0b06b73
Alexis Montes <montes.alexis25@gmail.com>
2025-10-10 02:19:25 -0400

    Updated main element to change the app template depending on if the layout is the standard one or the full page auth layout

--- kitsume-komix-client/src/App.vue
+++ kitsume-komix-client/src/App.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router';
+import { RouterView, useRoute } from 'vue-router';
 import SideBar from './components/SideBar.vue';
 import TopBar from './components/TopBar.vue';
 
+const $route = useRoute();
 </script>
 
 <template>
-
-  <main class="flex flex-row h-screen w-full overflow-hidden">
-    <SideBar class="w-64 bg-sky-950 text-white flex-shrink-0"/>
-    <div class="flex-1 flex flex-col min-w-0">
-      <TopBar />
-      <RouterView class="flex-1 overflow-auto" />
-    </div>
+  <main class="h-screen w-full overflow-hidden">
+    <template v-if="$route.meta.layout !== 'auth'">
+      <div class="flex flex-row h-full">
+        <SideBar class="w-64 bg-sky-950 text-white flex-shrink-0" />
+        <div class="flex-1 flex flex-col min-w-0">
+          <TopBar />
+          <RouterView class="flex-1 overflow-auto" />
+        </div>
+      </div>
+    </template>
+    <template v-else>
+      <RouterView class="h-full w-full" />
+    </template>
   </main>
-    
 </template>
 
 <style scoped>
