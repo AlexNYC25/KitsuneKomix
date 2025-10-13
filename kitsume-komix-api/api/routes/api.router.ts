c041dbf81ddf48c9786e7ba84a1046847fae2622
Alexis Montes <montes.alexis25@gmail.com>
2025-10-12 22:58:39 -0400

    added the image router to the api router

--- kitsume-komix-api/api/routes/api.router.ts
+++ kitsume-komix-api/api/routes/api.router.ts
@@ -8,6 +8,7 @@ import comicSeriesRouter from "./api.comicseries.router.ts";
 import comicPagesRouter from "./api.comicpages.router.ts";
 import collectionsRouter from "./api.collections.router.ts";
 import readlistsRouter from "./api.readlists.router.ts";
+import imageRouter from "./image.router.ts";
 
 const apiRouter = new OpenAPIHono();
 
@@ -20,5 +21,6 @@ apiRouter.route("/comic-series", comicSeriesRouter);
 apiRouter.route("/comic-pages", comicPagesRouter);
 apiRouter.route("/collections", collectionsRouter);
 apiRouter.route("/readlists", readlistsRouter);
+apiRouter.route("/image", imageRouter);
 
 export default apiRouter;
