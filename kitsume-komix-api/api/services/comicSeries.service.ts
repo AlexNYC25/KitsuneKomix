53ec15a49f43aa41cb5ce1bde0e4c29581b9a774
Alexis Montes <montes.alexis25@gmail.com>
2025-10-17 23:08:06 -0400

    filled in service function for upedated series

--- kitsume-komix-api/api/services/comicSeries.service.ts
+++ kitsume-komix-api/api/services/comicSeries.service.ts
@@ -1,6 +1,6 @@
 
 import { getUsersComicLibraries } from "../../db/sqlite/models/comicLibraries.model.ts";
-import { getLatestComicSeries } from "../../db/sqlite/models/comicSeries.model.ts";
+import { getLatestComicSeries, getUpdatedComicSeries } from "../../db/sqlite/models/comicSeries.model.ts";
 import { getComicBooksBySeriesId } from "../../db/sqlite/models/comicBooks.model.ts";
 import { getThumbnailsByComicBookId } from "../../db/sqlite/models/comicBookThumbnails.model.ts";
 import type { ComicSeries } from "../../types/index.ts";
@@ -40,10 +40,39 @@ export const getLatestComicSeriesUserCanAccess = async (
 		}
 	}
 
-  // then we want to expose this image URL in the response, for now we return the file path directly, and 
-	// set up a different endpoint to serve the image properly
-	// in the future we might want to generate resized thumbnails and store those paths instead
-
-  // Finally, return the list of comic series
   return latestSeriesWithThumbnails;
+}
+
+export const getUpdatedComicSeriesUserCanAccess = async (
+  userId: number,
+  limit: number = 20,
+  offset: number = 0,
+): Promise<Array<ComicSeriesWithThumbnail>> => {
+  const userLibraries = await getUsersComicLibraries(userId);
+
+  const libraryIds = userLibraries.map(lib => lib.id);
+  const updatedSeries = await getUpdatedComicSeries(limit, offset, libraryIds);
+
+  const updatedSeriesWithThumbnails: Array<ComicSeriesWithThumbnail> = [];
+
+  for (const series of updatedSeries) {
+    const comicBooksForCurrentSeries = await getComicBooksBySeriesId(series.id);
+    if (comicBooksForCurrentSeries.length === 0) {
+      // No comic books in this series, skip to next series
+      continue;
+    }
+
+    const firstComicBook = comicBooksForCurrentSeries[0];
+
+    const thumbnails = await getThumbnailsByComicBookId(firstComicBook.id);
+    if (thumbnails && thumbnails.length > 0) {
+      const seriesWithThumbnailUrl = series as ComicSeriesWithThumbnail;
+      seriesWithThumbnailUrl.thumbnailUrl = thumbnails[0].file_path.replace(CACHE_DIRECTORY, "/api/image");
+      updatedSeriesWithThumbnails.push(seriesWithThumbnailUrl);
+    } else {
+      updatedSeriesWithThumbnails.push(series as ComicSeriesWithThumbnail);
+    }
+  }
+
+  return updatedSeriesWithThumbnails;
 }
