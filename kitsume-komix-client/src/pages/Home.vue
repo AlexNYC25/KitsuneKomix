33c22b7be9e01cbadd9ca80ce9e4e5f3148f528a
Alexis Montes <montes.alexis25@gmail.com>
2025-10-20 14:34:30 -0400

    Updated home page to instead use imported component

--- kitsume-komix-client/src/pages/Home.vue
+++ kitsume-komix-client/src/pages/Home.vue
@@ -3,11 +3,8 @@ import { ref, onMounted, computed } from 'vue';
 import { useHomeStore } from '@/stores/home';
 import { useAuthStore } from '@/stores/auth';
 
-import { Galleria } from 'primevue';
-import Carousel from 'primevue/carousel';
-import Button from 'primevue/button';
-
 import ComicSeriesCarousel from '@/components/ComicSeriesCarousel.vue';
+import HomePageGalleria from '@/components/HomePageGalleria.vue';
 
 const homeStore = useHomeStore();
 const authStore = useAuthStore();
@@ -27,75 +24,12 @@ onMounted(async () => {
 const latestSeries = computed(() => homeStore.getLatestSeries);
 const updatedSeries = computed(() => homeStore.getUpdatedSeries);
 
-const responsiveOptions = ref([
-	{
-		breakpoint: '991px',
-		numVisible: 3
-	},
-	{
-		breakpoint: '767px',
-		numVisible: 3
-	},
-	{
-		breakpoint: '575px',
-		numVisible: 1
-	}
-]);
-const images = ref(
-	[
-		{
-			itemImageSrc: 'https://placehold.co/1600x400/blue/white',
-			thumbnailImageSrc: 'https://placehold.co/600x400/blue/white',
-			alt: 'Description for Image 1',
-			title: 'Title 1'
-		},
-		{
-			itemImageSrc: 'https://placehold.co/1600x400/orange/white',
-			thumbnailImageSrc: 'https://placehold.co/600x400/orange/white',
-			alt: 'Description for Image 2',
-			title: 'Title 2'
-		},
-		{
-			itemImageSrc: 'https://placehold.co/1600x400/yellow/white',
-			thumbnailImageSrc: 'https://placehold.co/600x400/yellow/white',
-			alt: 'Description for Image 3',
-			title: 'Title 3'
-		},
-		{
-			itemImageSrc: 'https://placehold.co/1600x400/green/white',
-			thumbnailImageSrc: 'https://placehold.co/600x400/green/white',
-			alt: 'Description for Image 4',
-			title: 'Title 4'
-		},
-		{
-			itemImageSrc: 'https://placehold.co/1600x400/black/white',
-			thumbnailImageSrc: 'https://placehold.co/600x400/black/white',
-			alt: 'Description for Image 5',
-			title: 'Title 5'
-		},
-		{
-			itemImageSrc: 'https://placehold.co/1600x400/purple/white',
-			thumbnailImageSrc: 'https://placehold.co/600x400/purple/white',
-			alt: 'Description for Image 6',
-			title: 'Title 6'
-		}
-	]
-)
-
 </script>
 
 <template>
 	<div class="home flex flex-col w-full overflow-auto p-4">
 		<div class="card w-full">
-			<Galleria :value="images" :responsiveOptions="responsiveOptions" :numVisible="5" :circular="false"
-				:showItemNavigators="true" :showThumbnails="false" :showItemNavigatorsOnHover="true" :showIndicators="true">
-				<template #item="slotProps" class="flex justify-center">
-					<img :src="slotProps.item.itemImageSrc" :alt="slotProps.item.alt" />
-				</template>
-				<template #thumbnail="slotProps">
-					<img :src="slotProps.item.thumbnailImageSrc" :alt="slotProps.item.alt" />
-				</template>
-			</Galleria>
+			<HomePageGalleria />
 		</div>
 
 		<div id="home-latest-series" class="card w-full">
