1950064f4c186b788b0910a14ba22902c767345c
Alexis Montes <montes.alexis25@gmail.com>
2025-10-20 23:51:30 -0400

    Updated placeholder file with start of actual placeholder

--- kitsume-komix-client/src/pages/ComicSeries.vue
+++ kitsume-komix-client/src/pages/ComicSeries.vue
@@ -3,7 +3,37 @@
 </script>
 
 <template>
-	<div>
-		Placeholder for ComicSeries 
+	<div
+		class="comic-series-page flex flex-col w-full h-full p-4 overflow-auto"
+	>
+		<div
+			class="comic-series-page-details bg-cyan-800 h-120 w-full rounded-2xl flex"
+		>
+			<!-- Placeholder for ComicSeries details -->
+			<div class="comic-series-page-details-thumbnail h-full rounded-l-2xl ">
+				<img
+					src="https://placehold.co/400x600/grey/white?text=Comic+Series+Thumbnail"
+					alt="Comic Series Thumbnail"
+					class="object-contain h-full pl-5 py-5"
+				/>
+			</div>
+
+			<div 
+				class="comic-series-page-details-info h-full m-6"
+			>
+				<div class="comic-series-page-details-title text-shadow-lg font-bold text-4xl">
+					Comic Series Title
+				</div>
+			</div>
+		</div>
+
+		<div
+			class="comic-series-page-contents"
+		>
+			<!-- Placeholder for ComicSeries contents such as list of comics -->
+			<div>
+
+			</div>
+		</div>
 	</div>
 </template>
