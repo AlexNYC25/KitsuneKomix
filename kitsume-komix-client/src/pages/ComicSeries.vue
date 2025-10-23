e088d08e3e1c0bfdfcd9087b4f34970b419e9c6f
Alexis Montes <montes.alexis25@gmail.com>
2025-10-23 03:04:32 -0400

    Updated comic series page to start using the metadata provided and show details

--- kitsume-komix-client/src/pages/ComicSeries.vue
+++ kitsume-komix-client/src/pages/ComicSeries.vue
@@ -36,10 +36,14 @@ onMounted(async () => {
 			<!-- Placeholder for ComicSeries details -->
 			<div class="comic-series-page-details-thumbnail h-full rounded-l-2xl ">
 				<img
-					src="https://placehold.co/400x600/grey/white?text=Comic+Series+Thumbnail"
+					:src="'http://localhost:8000' + comicSeriesData?.thumbnailUrl || 'https://via.placeholder.com/300x450?text=No+Image'"
 					alt="Comic Series Thumbnail"
 					class="object-contain h-full pl-5 py-5"
 				/>
+			</div>
+
+			<div class="comic-series-page-details-content-details">
+
 			</div>
 
 			<div 
@@ -50,6 +54,127 @@ onMounted(async () => {
 				</div>
 				<div class="comic-series-page-details-description mt-4 text-lg">
 					{{ comicSeriesData?.description }}
+				</div>
+
+				<div
+					class="comic-series-page-details-stats flex"
+				>
+					<!-- Placeholder for ComicSeries stats such as year x of y issue, type i.e. ongoing or limited -->
+				</div>
+
+				<div
+					class="comic-series-page-details-contents"
+				>
+					<div
+						class="comic-series-page-detail-contents-characters flex flex-wrap w-full"
+					>
+						<div class="font-bold">Characters:&nbsp;</div>
+						<div 
+							v-for="(value, index) in comicSeriesData?.metadata.characters.split(',')" 
+							:key="index"
+							class="comic-series-page-detail-credit-item"
+						>
+							<div>{{ value }},&nbsp;</div>
+						</div>
+					</div>
+
+
+					<div
+						class="comic-series-page-detail-contents-teams flex flex-wrap w-full"
+					>
+						<div class="font-bold">Teams:&nbsp;</div>
+						<div 
+							v-for="(value, index) in comicSeriesData?.metadata.teams.split(',')" 
+							:key="index"
+							class="comic-series-page-detail-credit-item"
+						>
+							<div>{{ value }},&nbsp;</div>
+						</div>
+					</div>
+				</div>
+
+				<div
+					class="comic-series-page-detail-credits mt-4"
+				>
+					<div
+						class="comic-series-page-detail-credits-writers flex flex-wrap"
+					>
+						<div class="font-bold">Writers:&nbsp;</div>
+						<div 
+							v-for="(value, index) in comicSeriesData?.metadata.writers.split(',')" 
+							:key="index"
+							class="comic-series-page-detail-credit-item"
+						>
+							<div>{{ value }},&nbsp;</div>
+						</div>
+					</div>
+						
+
+					<div
+						class="comic-series-page-detail-credits-colorists flex"
+					>
+						<div class="font-bold">Colorists:&nbsp;</div>
+						<div
+							v-for="(value, index) in comicSeriesData?.metadata.colorists.split(',')"
+							:key="index"
+							class="comic-series-page-detail-credit-item"
+						>
+							<div>{{ value }},&nbsp;</div>
+						</div>
+					</div>
+
+					<div
+						class="comic-series-page-detail-credits-cover-artists flex flex-wrap"
+					>
+						<div class="font-bold overflow-auto">Cover Artists:&nbsp;</div>
+						<div
+							v-for="(value, index) in comicSeriesData?.metadata.coverArtists.split(',')"
+							:key="index"
+							class="comic-series-page-detail-credit-item"
+						>
+							<div>{{ value }},&nbsp;</div>
+						</div>
+					</div>
+
+
+					<div
+						class="comic-series-page-detail-credits-inkers flex"
+					>
+						<div class="font-bold">Inkers:&nbsp;</div>
+						<div
+							v-for="(value, index) in comicSeriesData?.metadata.inkers.split(',')"
+							:key="index"
+							class="comic-series-page-detail-credit-item"
+						>
+							<div>{{ value }},&nbsp;</div>
+						</div>
+					</div>
+
+					<div
+						class="comic-series-page-detail-credits-letterers flex"
+					>
+						<div class="font-bold">Letterers:&nbsp;</div>
+						<div
+							v-for="(value, index) in comicSeriesData?.metadata.letterers.split(',')"
+							:key="index"
+							class="comic-series-page-detail-credit-item"
+						>
+							<div>{{ value }},&nbsp;</div>
+						</div>
+					</div>
+
+					<div
+						class="comic-series-page-detail-credits-editors flex"
+					>
+						<div class="font-bold">Editors:&nbsp;</div>
+						<div
+							v-for="(value, index) in comicSeriesData?.metadata.editors.split(',')"
+							:key="index"
+							class="comic-series-page-detail-credit-item"
+						>
+							<div>{{ value }},&nbsp;</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
