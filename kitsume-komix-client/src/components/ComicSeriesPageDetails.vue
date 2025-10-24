e490e8745d2f7d5549d1152b58fae8ac57a6d034
Alexis Montes <montes.alexis25@gmail.com>
2025-10-23 22:42:39 -0400

    Created reusable component to displat metadata details on comic series page

---
+++ kitsume-komix-client/src/components/ComicSeriesPageDetails.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+	defineProps(['comicMetadataDetailsLabel', 'comicMetadataDetails'])
+</script>
+
+<template>
+  <div class="font-bold">{{ comicMetadataDetailsLabel }}:&nbsp;</div>
+		<div 
+			v-for="(value, index) in comicMetadataDetails?.split(',')" 
+			:key="index"
+			class="comic-series-page-detail-item"
+		>
+			<div>{{ value }},&nbsp;</div>
+	</div>
+</template>
