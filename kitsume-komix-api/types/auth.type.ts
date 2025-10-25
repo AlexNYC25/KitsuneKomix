8cfad99d417d60e1e2d13c48f10bbbdb8a2f4dd4
Alexis Montes <montes.alexis25@gmail.com>
2025-10-25 16:07:36 -0400

    added comment to auth request type

--- kitsume-komix-api/types/auth.type.ts
+++ kitsume-komix-api/types/auth.type.ts
@@ -1,6 +1,10 @@
 import type { AccessClaims } from "../auth/auth.ts";
 import type { JWTPayload } from "jose";
 
+/**
+ * AppEnv type to include user information in the environment variables.
+ * Important for setting up Hono+OpenApi routes with authentication context.
+ */
 export type AppEnv = {
   Variables: {
     user?: AccessClaims & JWTPayload;
