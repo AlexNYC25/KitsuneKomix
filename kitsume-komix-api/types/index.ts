36184a377b0cd859bec64f77b0c329602fb85c42
Alexis Montes <montes.alexis25@gmail.com>
2025-10-25 23:06:29 -0400

    removed re export decleration for file that no longer exists

--- kitsume-komix-api/types/index.ts
+++ kitsume-komix-api/types/index.ts
@@ -5,7 +5,6 @@ export * from "./database.types.ts";
 export * from "./user.type.ts";
 export * from "./comicLibrary.type.ts";
 export * from "./comicBook.type.ts";
-export * from "./comicSeries.type.ts";
 
 // Re-export auth types
 export * from "./auth.type.ts";
