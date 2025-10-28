f0c87cbd0754fc54491ee46a74bf4abb866f953a
Alexis Montes <montes.alexis25@gmail.com>
2025-10-28 16:33:22 -0400

    migrated from using manually defined schemas to that generated by drizzle-zod plugin package

--- kitsume-komix-api/zod/schemas/data/comic-series.schema.ts
+++ kitsume-komix-api/zod/schemas/data/comic-series.schema.ts
@@ -1,5 +1,49 @@
 import { z } from "@hono/zod-openapi";
-import { ComicBookWithThumbnailSchema } from "./comic-books.schema.ts";
+import { comicBookSelectJoinedWithThumbnailSchema } from "./comic-books.schema.ts";
+
+import { comicSeriesTable } from "../../../db/sqlite/schema.ts";
+import { createSelectSchema } from "../../factory.ts";
+
+export const comicSeriesSelectSchema: z.ZodObject =
+	createSelectSchema(comicSeriesTable)
+		.openapi("ComicSeriesSelectSchema");
+
+export const comicSeriesSelectJoinedWithThumbnailSchema =
+	createSelectSchema(comicSeriesTable)
+		.extend({
+			thumbnailUrl: z.string().nullable(),
+		})
+		.openapi("ComicSeriesSelectJoinedWithThumbnailSchema");
+
+export const comicSeriesSelectJoinedWithThumbnailAndMetadataSchema =
+	comicSeriesSelectJoinedWithThumbnailSchema.extend({
+		metadata: z.object({
+			writers: z.string().nullable(),
+			pencillers: z.string().nullable(),
+			inkers: z.string().nullable(),
+			colorists: z.string().nullable(),
+			letterers: z.string().nullable(),
+			editors: z.string().nullable(),
+			coverArtists: z.string().nullable(),
+			publishers: z.string().nullable(),
+			imprints: z.string().nullable(),
+			genres: z.string().nullable(),
+			characters: z.string().nullable(),
+			teams: z.string().nullable(),
+			locations: z.string().nullable(),
+			storyArcs: z.string().nullable(),
+			seriesGroups: z.string().nullable(),
+		}).or(z.record(z.string(), z.never())), // Allow either full metadata object or an empty object
+	})
+	.openapi("ComicSeriesSelectJoinedWithThumbnailAndMetadataSchema");
+
+export const comicSeriesSelectJoinedWithThubnailsMetadataAndComicsSchema =
+	comicSeriesSelectJoinedWithThumbnailAndMetadataSchema.extend({
+		comics: z.array(
+			comicBookSelectJoinedWithThumbnailSchema
+		),
+	})
+	.openapi("ComicSeriesSelectJoinedWithThubnailsMetadataAndComicsSchema");
 
 /**
  * Schema representing a comic series with its thumbnail URL.
@@ -7,7 +51,7 @@ import { ComicBookWithThumbnailSchema } from "./comic-books.schema.ts";
  * This schema is used in responses where comic series data along with their
  * Based on the ComicSeries type from the schema definition with the addition of the thumbnailUrl field.
  */
-export const comicSeriesWithThumbnailsSchema = z.object({
+export const z_comicSeriesWithThumbnailsSchema = z.object({
 	id: z.number(),
 	name: z.string(),
 	description: z.string().nullable(),
@@ -17,49 +61,4 @@ export const comicSeriesWithThumbnailsSchema = z.object({
 	thumbnailUrl: z.string().nullable(),
 });
 
-/**
- * Schema representing a comic series with its thumbnail URL and metadata.
- * 
- * This schema is used in responses where comic series data along with their
- * Based on the ComicSeries type from the schema definition with the addition of the thumbnailUrl field
- * and a metadata object that may be empty or contain full metadata information.
- * 
- * NOTE: was previously used as the primary response schema for comic series details,
- * but has been superseded by comicSeriesWithComicsMetadataAndThumbnailsSchema.
- */
-export const comicSeriesWithMetadataAndThumbnailsSchema = comicSeriesWithThumbnailsSchema.extend({
-	metadata: z.object({
-		writers: z.string().nullable(),
-		pencillers: z.string().nullable(),
-		inkers: z.string().nullable(),
-		colorists: z.string().nullable(),
-		letterers: z.string().nullable(),
-		editors: z.string().nullable(),
-		coverArtists: z.string().nullable(),
-		publishers: z.string().nullable(),
-		imprints: z.string().nullable(),
-		genres: z.string().nullable(),
-		characters: z.string().nullable(),
-		teams: z.string().nullable(),
-		locations: z.string().nullable(),
-		storyArcs: z.string().nullable(),
-		seriesGroups: z.string().nullable(),
-	}).or(z.record(z.string(), z.never())), // Allow either full metadata object or an empty object
-});
 
-/**
- * Schema representing a comic series with its thumbnail URL, metadata, and associated comics with their thumbnails.
- * 
- * This schema is used in responses where comic series data along with their books and metadata are returned.
- * Based on the ComicSeries type from the schema definition with the addition of the thumbnailUrl field,
- * a metadata object that may be empty or contain full metadata information, and a comics object containing
- * the total number of comics and an array of comic books each with their own thumbnail URL.
- */
-export const comicSeriesWithComicsMetadataAndThumbnailsSchema = comicSeriesWithMetadataAndThumbnailsSchema.extend({
-	comics: z.object({
-		total: z.number(),
-		books: z.array(
-			ComicBookWithThumbnailSchema
-		),
-	}),
-});
