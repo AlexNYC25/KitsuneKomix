f0be3e5e3ea2d0827d9fade4590a9e6c00e3e94b
Alexis Montes <montes.alexis25@gmail.com>
2025-10-31 02:14:44 -0400

    moved healthcheck route to it's own file/new router

---
+++ kitsume-komix-api/api/routes/health.router.ts
@@ -0,0 +1,24 @@
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
+import { MessageResponseSchema } from "../../zod/schemas/response.schema.ts";
+
+const healthRouter = new OpenAPIHono();
+
+healthRouter.openapi(
+  createRoute({
+		method: "get",
+		path: "/",
+		summary: "Health check",
+		tags: ["Root"],
+		responses: {
+			200: {
+				content: { "application/json": { schema: MessageResponseSchema } },
+				description: "Health check",
+			},
+		},
+	}),
+	(_c) => {
+		return _c.json({ message: "Health check passed", status: "ok" });
+	}
+);
+
+export default healthRouter;
