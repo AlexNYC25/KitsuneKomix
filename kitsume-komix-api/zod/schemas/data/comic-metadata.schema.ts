a8b3d9806afaf10e96c993fae42b9f152c6efc2e
Alexis Montes <montes.alexis25@gmail.com>
2025-11-06 02:18:54 -0500

    Seperated the comic metadata structure into it's own file

---
+++ kitsume-komix-api/zod/schemas/data/comic-metadata.schema.ts
@@ -0,0 +1,28 @@
+import { z } from "@hono/zod-openapi";
+
+/**
+ * Schema for comic series metadata
+ * 
+ * Includes various optional fields for comic series information from the various
+ * metadata categories from their respective databases tables.
+ */
+export const metadataSchema = z.object({
+  writers: z.string().nullable().optional(),
+  pencillers: z.string().nullable().optional(),
+  inkers: z.string().nullable().optional(),
+  colorists: z.string().nullable().optional(),
+  letterers: z.string().nullable().optional(),
+  editors: z.string().nullable().optional(),
+  coverArtists: z.string().nullable().optional(),
+  publishers: z.string().nullable().optional(),
+  imprints: z.string().nullable().optional(),
+  genres: z.string().nullable().optional(),
+  characters: z.string().nullable().optional(),
+  teams: z.string().nullable().optional(),
+  locations: z.string().nullable().optional(),
+  storyArcs: z.string().nullable().optional(),
+  seriesGroups: z.string().nullable().optional(),
+}).openapi({
+  title: "ComicSeriesMetadata",
+  description: "Metadata for a comic series",
+});
