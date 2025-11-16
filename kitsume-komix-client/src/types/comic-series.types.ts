9a5feb8b9cd25a398bf5a18fea7d6476a1f6cc38
Alexis Montes <montes.alexis25@gmail.com>
2025-11-16 11:25:05 -0500

    added more additional comic-series types from variouse endpoints

--- kitsume-komix-client/src/types/comic-series.types.ts
+++ kitsume-komix-client/src/types/comic-series.types.ts
@@ -1,7 +1,26 @@
 import type { paths } from '../openapi/openapi-schema';
 
+// /comic-series/{id} endpoint types
 export type GetComicSeriesByIdResponse = paths['/comic-series/{id}']['get']['responses']['200']['content']['application/json'];
 
 export type ComicSeriesWithComics = GetComicSeriesByIdResponse['data'];
 
 export type ComicSeriesWithComicsSingleComic = ComicSeriesWithComics["comics"][number];
+
+// /comic-series/latest endpoint types
+export type GetLatestComicSeriesResponse = paths['/comic-series/latest']['get']['responses']['200']['content']['application/json'];
+
+export type LatestComicSeriesData = GetLatestComicSeriesResponse['data'];
+
+export type LatestComicSeriesSingle = LatestComicSeriesData[number];
+
+export type LatestComicSeriesMeta = GetLatestComicSeriesResponse['meta'];
+
+// /comic-series/updated endpoint types
+export type GetUpdatedComicSeriesResponse = paths['/comic-series/updated']['get']['responses']['200']['content']['application/json'];
+
+export type UpdatedComicSeriesData = GetUpdatedComicSeriesResponse['data'];
+
+export type UpdatedComicSeriesSingle = UpdatedComicSeriesData[number];
+
+export type UpdatedComicSeriesMeta = GetUpdatedComicSeriesResponse['meta'];
