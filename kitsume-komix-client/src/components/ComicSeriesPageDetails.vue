e6f7d6f2dbd30e95b0b93f25ec1ce1d394c94518
Alexis Montes <montes.alexis25@gmail.com>
2025-11-22 16:33:54 -0500

    Updated comic page details area to only show a few chips with the option to expand for more

--- kitsume-komix-client/src/components/ComicSeriesPageDetails.vue
+++ kitsume-komix-client/src/components/ComicSeriesPageDetails.vue
@@ -1,18 +1,50 @@
 <script setup lang="ts">
+	import { ref, computed } from 'vue';
 	import Chip from 'primevue/chip';
+	import Button from 'primevue/button';
 
-	defineProps(['comicMetadataDetailsLabel', 'comicMetadataDetails'])
+	const props = defineProps({
+		comicMetadataDetailsLabel: String,
+		comicMetadataDetails: String,
+		maxVisible: { type: Number, default: 5 }
+	});
+
+	const isExpanded = ref(false);
+
+	const items = computed(() => {
+		return props.comicMetadataDetails?.split(',').map((item: string) => item.trim()) || [];
+	});
+
+	const visibleItems = computed(() => {
+		return isExpanded.value ? items.value : items.value.slice(0, props.maxVisible);
+	});
+
+	const hasMore = computed(() => items.value.length > props.maxVisible);
+	const hiddenCount = computed(() => items.value.length - props.maxVisible);
 </script>
 
 <template>
-	<div class="flex flex-wrap w-full items-center">
-		<div class="font-bold">{{ comicMetadataDetailsLabel }}:&nbsp;</div>
-		<div 
-			v-for="(value, index) in comicMetadataDetails?.split(',')" 
-			:key="index"
-			class="comic-series-page-detail-item"
-		>
-			<Chip :label="value" class="m-1" />
+	<div class="flex flex-col gap-2 w-full">
+		<div class="flex flex-wrap items-center">
+			<div class="font-bold">{{ comicMetadataDetailsLabel }}:&nbsp;</div>
+			<div 
+				v-for="(value, index) in visibleItems" 
+				:key="index"
+				class="comic-series-page-detail-item"
+			>
+				<Chip :label="value" class="m-1" />
+			</div>
+		</div>
+		
+		<!-- Show More/Less Button -->
+		<div v-if="hasMore" class="flex items-center">
+			<Button 
+				:label="isExpanded ? 'Show Less' : `Show More (+${hiddenCount})`"
+				@click="isExpanded = !isExpanded"
+				text
+				size="small"
+				class="text-cyan-400 hover:text-cyan-300"
+			/>
 		</div>
 	</div>
 </template>
