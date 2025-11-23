b50dde3e404471feb9c6c116584e25b2953b005e
Alexis Montes <montes.alexis25@gmail.com>
2025-11-23 13:09:59 -0500

    Updated the header color to match the rest of the app

--- kitsume-komix-client/src/components/TopBar.vue
+++ kitsume-komix-client/src/components/TopBar.vue
@@ -3,17 +3,17 @@
 </script>
 
 <template>
-	<header class="flex justify-between items-center p-4 bg-amber-400">
+	<header class="flex justify-between items-center p-4 bg-gray-800 border-b border-gray-700">
 		<div id="top-bar-left">
 			<div>
-				<p>Welcome, User!</p>
+				<p class="text-white font-semibold">Welcome, User!</p>
 			</div>
 		</div>
 		<div id="top-bar-right" class="flex-shrink ml-auto max-w-lg">
-			<InputGroup class="bg-blue-100">
-				<InputText placeholder="Keyword" />
-				<InputGroupAddon>
-					<i class="pi pi-search" />
+			<InputGroup class="bg-gray-700">
+				<InputText placeholder="Keyword" class="bg-gray-700 text-white placeholder-gray-400" />
+				<InputGroupAddon class="bg-gray-700">
+					<i class="pi pi-search text-gray-400" />
 				</InputGroupAddon>
 			</InputGroup>
 		</div>
