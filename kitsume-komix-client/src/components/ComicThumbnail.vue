d76c09f7a4b64c995bd1f7c2d4a4d4a59a396ee2
Alexis Montes <montes.alexis25@gmail.com>
2025-11-25 14:03:28 -0500

    Created a standalone component for displaying the thumbnail for both the comic series and comic book thumbnail

---
+++ kitsume-komix-client/src/components/ComicThumbnail.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import { composeStaticUrl } from '@/utilities/apiClient';
+
+const props = defineProps<{
+  thumbnailUrl?: string;
+  comicName?: string;
+}>();
+</script>
+
+<template>
+    <div class="flex-shrink-0 w-70 h-full">
+			<div class=" bg-gray-900 rounded-lg overflow-hidden flex items-center justify-center">
+				<img
+					v-if="thumbnailUrl"
+					:src=composeStaticUrl(thumbnailUrl)
+					:alt="comicName || 'Series Thumbnail'"
+					class="w-full h-full object-contain"
+				/>
+				<div v-else class="w-full h-full bg-gray-700 flex items-center justify-center">
+					<span class="text-gray-500">No Image</span>
+				</div>
+			</div>
+		</div>
+</template>
