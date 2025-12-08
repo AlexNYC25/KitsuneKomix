bd5908137bec630d0a736c14458a1824cd62e2c2
Alexis Montes <montes.alexis25@gmail.com>
2025-12-07 19:13:24 -0500

    Added types for the expected comic book data from the /comic-books/:id/metadata route

--- kitsume-komix-client/src/types/comic-books.types.ts
+++ kitsume-komix-client/src/types/comic-books.types.ts
@@ -10,3 +10,36 @@ export type ComicBook = ComicBookInSeriesData[number];
 export type ComicBooksSeriesMeta = GetComicsBySeries['meta'];
 
 export type ComicBooksSeriesResponse = GetComicsBySeries;
+
+// Creator/Person interface for metadata fields
+export interface Creator {
+	id: number;
+	name: string;
+}
+
+// /comic-books/:id/metadata endpoint types
+export interface ComicBookMetadata {
+	id: number;
+	issueNumber: number;
+	title: string;
+	description?: string | null;
+	publicationDate?: string | null;
+	pageCount: number;
+	filePath: string;
+	series?: string | null;
+	writers: Creator[];
+	pencillers: Creator[];
+	inkers: Creator[];
+	letterers: Creator[];
+	colorists: Creator[];
+	editors: Creator[];
+	coverArtists: Creator[];
+	publishers: Creator[];
+	imprints: Creator[];
+	genres: Creator[];
+	characters: Creator[];
+	teams: Creator[];
+	locations: Creator[];
+	storyArcs: Creator[];
+	seriesGroups: Creator[];
+}
