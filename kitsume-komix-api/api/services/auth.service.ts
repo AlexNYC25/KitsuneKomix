cd08a6297bb9f87e79462829a2fe214fbd0002ff
Alexis Montes <montes.alexis25@gmail.com>
2025-12-20 23:22:49 -0500

    Updated the auth service, comment doc, types and fixed the bug due to the update in the casing fro the db columns

--- kitsume-komix-api/api/services/auth.service.ts
+++ kitsume-komix-api/api/services/auth.service.ts
@@ -1,15 +1,26 @@
 import { getUserByEmail } from "../../db/sqlite/models/users.model.ts";
 
-import { verifyPassword } from "../../utilities/hash.ts";
+import { verifyPassword } from "#utilities/hash.ts";
+import { User } from "#types/index.ts";
 
-export const authenticateUser = async (email: string, password: string) => {
-  const user = await getUserByEmail(email);
+/**
+ * Authenticates a user by email and password.
+ * @param email User's email address
+ * @param password User's password
+ * @returns The authenticated user's details if successful
+ * @throws Error if authentication fails
+ */
+export const authenticateUser = async (
+  email: string, 
+  password: string
+): Promise<{ id: number; email: string; admin: boolean }> => {
+  const user: User | null = await getUserByEmail(email);
   if (!user) {
     throw new Error("User not found");
   }
 
   // Verify the password (you should hash and compare in a real app)
-  if (!(await verifyPassword(user.password_hash, password))) {
+  if (!(await verifyPassword(user.passwordHash, password))) {
     throw new Error("Invalid password");
   }
 
