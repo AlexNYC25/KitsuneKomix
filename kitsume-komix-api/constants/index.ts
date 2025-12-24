d62382ebb644c710e239c905900311f2eea07577
Alexis Montes <montes.alexis25@gmail.com>
2025-12-23 23:23:39 -0500

    Updated the index to reexport the queryable constants values as well

--- kitsume-komix-api/constants/index.ts
+++ kitsume-komix-api/constants/index.ts
@@ -1,2 +1,6 @@
 
+// Request related constants such as default pagination values
 export * from "./requests.ts";
+
+// DB table specific lookup constants for queryable columns specifiying filtering and sorting options
+export * from "./queryableColumns.ts";
