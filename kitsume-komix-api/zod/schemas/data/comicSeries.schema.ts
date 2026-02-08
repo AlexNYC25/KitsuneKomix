f4ecb0b9462c4a650f61fed57adef48d5d942d94
Alexis Montes <montes.alexis25@gmail.com>
2026-02-07 21:47:08 -0500

    Added comic books as an optional property as part of the comic series schema

--- kitsume-komix-api/zod/schemas/data/comicSeries.schema.ts
+++ kitsume-komix-api/zod/schemas/data/comicSeries.schema.ts
@@ -1,3 +1,5 @@
+import { z } from "@hono/zod-openapi";
+
 import {
   ComicSeriesSelectSchema,
   ComicBookThumbnailSelectSchema
@@ -5,6 +7,9 @@ import {
 import { 
   MetadataSchema 
 } from "./comicMetadata.schema.ts";
+import {
+  ComicBookSchema
+} from "./comicBooks.schema.ts";
 
 /**
  * Schema for comic series, extending the base ComicSeriesSelectSchema with additional fields for thumbnail and metadata. This schema represents the structure of a comic series as it will be returned in API responses, including optional fields for a thumbnail image and associated metadata.
@@ -13,6 +18,7 @@ export const ComicSeriesSchema = ComicSeriesSelectSchema.extend(
   {
     thumbnail: ComicBookThumbnailSelectSchema.shape.filePath.optional(),
     metadata: MetadataSchema.optional(),
+    comicBooks: z.array(ComicBookSchema).optional(),
   },
 ).openapi({
   title: "ComicSeries",
