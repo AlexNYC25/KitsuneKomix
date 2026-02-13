995c2229a4ae33faa5e087918906df5f67b7d952
Alexis Montes <montes.alexis25@gmail.com>
2026-02-12 22:39:11 -0500

    Added the master dynamic query to the comic series model file

--- kitsume-komix-api/db/sqlite/models/comicSeries.model.ts
+++ kitsume-komix-api/db/sqlite/models/comicSeries.model.ts
@@ -1,6 +1,8 @@
-import { desc, eq, inArray, sql } from "drizzle-orm";
+import { desc, eq, inArray, sql, asc, ilike } from "drizzle-orm";
+import { SQLiteColumn, SQLiteSelect } from "drizzle-orm/sqlite-core";
 
 import { getClient } from "../client.ts";
+
 import {
   comicBookCharactersTable,
   comicBookColoristsTable,
@@ -43,7 +45,117 @@ import type {
   ComicSeries,
   ComicSeriesWithMetadata,
   NewComicSeries,
+  ComicSeriesFilteringAndSortingParams,
+  ComicSeriesFilterItem,
+  ComicSeriesSortField,
 } from "#types/index.ts";
+import { PAGE_SIZE_DEFAULT } from "../../../constants/index.ts";
+
+/**
+ * Exclusive dynamic filtering function specifcally for getComicSeriesWithMetadataFilteringSorting
+ * This is necessary as the filtering can be applied to any of the fields in the comic series table and we need to dynamically apply it to the query builder.
+ * @param filter 
+ * @param query 
+ * @returns the query with the filter applied
+ */
+const addFilteringToQuery = <T extends SQLiteSelect>(filter: ComicSeriesFilterItem, query: T): T => {
+  const { filterProperty, filterValue } = filter;
+
+  switch (filterProperty) {
+    case "id":
+      query.where(eq(comicSeriesTable.id, Number(filterValue)));
+      break;
+    case "name":
+      query.where(ilike(comicSeriesTable.name, `%${filterValue}%`));
+      break;
+    case "description":
+      query.where(ilike(comicSeriesTable.description, `%${filterValue}%`));
+      break;
+  }
+
+  return query;
+};
+
+/**
+ * Exclusive dynamic sorting function specifcally for getComicSeriesWithMetadataFilteringSorting
+ * This is necessary as the sorting can be applied to any of the fields in the comic series table and we need to dynamically apply it to the query builder.
+ * @param sortProperty 
+ * @param sortDirection 
+ * @param query 
+ * @returns the query with the sorting applied
+ */
+const addSortingToQuery = <T extends SQLiteSelect>(sortProperty: ComicSeriesSortField, sortDirection: string, query: T): T => {
+  const direction = sortDirection === "asc" ? asc : desc;
+
+  switch (sortProperty) {
+    case "id":
+      query.orderBy(direction(comicSeriesTable.id));
+      break;
+    case "name":
+      query.orderBy(direction(comicSeriesTable.name));
+      break;
+    case "createdAt":
+      query.orderBy(direction(comicSeriesTable.createdAt));
+      break;
+    case "updatedAt":
+      query.orderBy(direction(comicSeriesTable.updatedAt));
+      break;
+  }
+
+  return query;
+};
+
+/**
+ * Gets comic series with metadata filtering and sorting
+ * @param serviceDetails - Filtering and sorting parameters
+ * @returns Promise resolving to an array of ComicSeries objects 
+ * 
+ * Note: This function can be used to filter and sort on the metadata fields as well but not return them. i.e. we can filter by writer but not return the writer data with the comic book.
+ * This metadata must be fetched separately after getting the comic books and attached to the comic book objects upstream.
+ */
+export const getComicSeriesWithMetadataFilteringSorting = async (
+  serviceDetails: ComicSeriesFilteringAndSortingParams,
+): Promise<ComicSeries[]> => {
+  const { db, client } = getClient();
+  
+  if (!db || !client) {
+    throw new Error("Database client is not initialized");
+  }
+
+  const offset = serviceDetails.offset || 0;
+  const limit = serviceDetails.limit || PAGE_SIZE_DEFAULT;
+
+  try {
+    let query = 
+      db.select(
+        {
+          id: comicSeriesTable.id,
+          name: comicSeriesTable.name,
+          description: comicSeriesTable.description,
+          folderPath: comicSeriesTable.folderPath,
+          createdAt: comicSeriesTable.createdAt,
+          updatedAt: comicSeriesTable.updatedAt,
+        }
+      ).from(comicSeriesTable)
+      .groupBy(comicSeriesTable.id)
+      .offset(offset)
+      .limit(limit)
+      .$dynamic();
+
+    if (serviceDetails.sort?.property && serviceDetails.sort.order) {
+      query = addSortingToQuery(serviceDetails.sort.property, serviceDetails.sort.order, query);
+    }
+
+    if (serviceDetails.filters && serviceDetails.filters.length > 0) {
+      query = addFilteringToQuery(serviceDetails.filters[0], query);
+    }
+
+    return query;
+  } catch (error) {
+    console.error("Error fetching comic series with metadata filtering and sorting:", error);
+    throw error;
+  }
+}
 
 /**
  * Inserts a new comic series into the database
