4ddac09807f49312480de447065fafb843664e80
Alexis Montes <montes.alexis25@gmail.com>
2026-02-14 23:18:58 -0500

    Added comic series multiple reponses types infered from the response schema

--- kitsume-komix-api/types/responses.type.ts
+++ kitsume-komix-api/types/responses.type.ts
@@ -1,6 +1,10 @@
 import { z } from "zod";
-import { ComicBookMultipleResponseSchema } from "#schemas/response.schema.ts";
+import { ComicBookMultipleResponseSchema, ComicSeriesMultipleResponseSchema } from "#schemas/response.schema.ts";
 
 export type ComicBookMultipleResponse = z.infer<typeof ComicBookMultipleResponseSchema>;
 export type ComicBookMultipleResponseData = ComicBookMultipleResponse["data"];
 export type ComicBookMultipleResponseMeta = ComicBookMultipleResponse["meta"];
+
+export type ComicSeriesMultipleResponse = z.infer<typeof ComicSeriesMultipleResponseSchema>;
+export type ComicSeriesMultipleResponseData = ComicSeriesMultipleResponse["data"];
+export type ComicSeriesMultipleResponseMeta = ComicSeriesMultipleResponse["meta"];
