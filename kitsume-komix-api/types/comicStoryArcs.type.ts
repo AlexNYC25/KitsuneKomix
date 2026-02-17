6f2363cc3d6f2d5b47c5de9453afeefdeac1c65c
Alexis Montes <montes.alexis25@gmail.com>
2026-02-16 19:57:53 -0500

    Created new types to represent sort/filter options for story arcs + data types for what is expected at the service level

--- kitsume-komix-api/types/comicStoryArcs.type.ts
+++ kitsume-komix-api/types/comicStoryArcs.type.ts
@@ -1,6 +1,26 @@
-import { ComicBookStoryArc, ComicStoryArc } from "./database.types.ts";
+import {  ComicStoryArc } from "./database.types.ts";
+import { QueryableColumns } from "../constants/index.ts";
+
+export type AllowedComicStoryArcFilterProperties = keyof typeof QueryableColumns["comicReadlists"]["filter"];
+
+export type AllowedComicStoryArcSortProperties = keyof typeof QueryableColumns["comicReadlists"]["sort"];
 
 // Comic Story Arc with associated comic book IDs
 export type ComicStoryArcWithComicIds = ComicStoryArc & {
   comicBookIds: number[];
+};
+
+export type ComicStoryArcFilterItem = {
+  filterProperty: AllowedComicStoryArcFilterProperties;
+  filterValue: string;
+};
+
+export type ComicStoryArcsFilteringAndSortingParams = {
+  filters?: ComicStoryArcFilterItem[];
+  sort?: {
+    property: AllowedComicStoryArcSortProperties;
+    order: "asc" | "desc";
+  };
+  offset?: number;
+  limit?: number;
 };
