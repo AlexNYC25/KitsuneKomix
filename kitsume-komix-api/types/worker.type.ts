ede37e36e409340766bf190eb4f07b836a653f32
Alexis Montes <montes.alexis25@gmail.com>
2026-02-24 00:53:32 -0500

    Added a new type to describe what the initial file check result will be for the first stage fo the comic worker

--- kitsume-komix-api/types/worker.type.ts
+++ kitsume-komix-api/types/worker.type.ts
@@ -1,6 +1,20 @@
+import { ComicBook } from "./database.types.ts";
+
 export type WorkerJob = {
   data: {
     filePath: string;
     metadata: object;
   };
+};
+
+/**
+ * Result type for checking if a file should be processed by the worker.
+ * - `shouldBeProcessed`: Indicates if the file needs processing.
+ * - `dbRecord`: Optional existing comic book record from the database (if found).
+ * - `hash`: Optional current hash of the file for comparison.
+ */
+export type WorkerFileCheckResult = {
+  shouldBeProcessed: boolean;
+  dbRecord?: ComicBook;
+  hash: string;
 };
