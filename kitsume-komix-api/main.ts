b32afdce4ed1f7bc254b274f7af509743c9bde93
Alexis Montes <montes.alexis25@gmail.com>
2026-03-04 00:17:13 -0500

    Updated the index file to import all of the new workers, updated the import paths to use the shortcuts

--- kitsume-komix-api/main.ts
+++ kitsume-komix-api/main.ts
@@ -1,10 +1,12 @@
 import app from "./api/app.ts";
 import { getWatcherManager } from "./file-watching/index.ts";
-import "./queue/workers/comicWorker.ts";
+import "./queue/workers/comicBook.worker.ts";
+import "./queue/workers/series.worker.ts";
+import "./queue/workers/file.worker.ts";
 
-import { runMigrations } from "./db/migrate.ts";
+import { runMigrations } from "#db/migrate.ts";
 
-import { API_PORT } from "./utilities/environment.ts";
+import { API_PORT } from "#utilities/environment.ts";
 
 await runMigrations();
 
