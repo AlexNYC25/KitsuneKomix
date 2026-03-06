b3f60c205618dd8e9dccdfd832befdd4773edde5
Alexis Montes <montes.alexis25@gmail.com>
2026-03-06 15:26:55 -0500

    Updated the import for dirname to use the more deno friendly version of it

--- kitsume-komix-api/queue/workers/series.worker.ts
+++ kitsume-komix-api/queue/workers/series.worker.ts
@@ -1,5 +1,5 @@
 import { Worker } from "bullmq";
-import { dirname } from "node:path";
+import { dirname } from "@std/path";
 import { redisConnection } from "#db/redis/redisConnection.ts";
 
 import { queueLogger } from "../../logger/loggers.ts";
