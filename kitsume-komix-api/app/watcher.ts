2a380f945240b4cb7e687ffa209c3b8fdc8246f7
Alexis Montes <montes.alexis25@gmail.com>
2026-03-24 23:12:03 -0400

    cleaned up watcher file

--- kitsume-komix-api/app/watcher.ts
+++ kitsume-komix-api/app/watcher.ts
@@ -1,14 +1,19 @@
 import chokidar from "chokidar";
 
+import { apiLogger } from "#infrastructure/logger/loggers.ts";
 import {
   getAllComicLibraries,
   getComicLibraryLastChangedTime,
 } from "#infrastructure/db/sqlite/models/comicLibraries.model.ts";
 import { orchestrateFile } from "#modules/queue/orchestrators/comic.orchestrator.ts";
-import { apiLogger } from "#infrastructure/logger/loggers.ts";
+
 
 let instance: WatchManager | null = null;
 
+/**
+ * Point of access for the WatchManager singleton instance. Ensures that only one instance of WatchManager exists throughout the application.
+ * @returns The singleton instance of WatchManager.
+ */
 export function getWatcherManager(): WatchManager {
   if (!instance) {
     instance = new WatchManager();
@@ -16,7 +21,13 @@ export function getWatcherManager(): WatchManager {
   return instance;
 }
 
+/**
+ * The actual WatchManager class that encapsulates the logic for watching file system changes in comic library paths. 
+ * It uses chokidar to monitor directories and react to file additions, changes, and deletions. 
+ * The manager also periodically refreshes the list of watched paths based on the current state of the comic libraries in the database.
+ */
 export class WatchManager {
+  // Defines the chokidar watcher with the specified options.
   private watcher = chokidar.watch([], {
     persistent: true,
     ignoreInitial: false,
@@ -28,6 +39,7 @@ export class WatchManager {
   private byPath = new Map<string, number>();
 
   constructor() {
+    // As part of the constructor we set up the event listeners for the chokidar watcher
     this.watcher
       .on("add", (path) => this.onFileAdded(path))
       .on("change", (path) => this.onFileChanged(path))
@@ -43,13 +55,28 @@ export class WatchManager {
       )
       .on("ready", () => apiLogger.info("Chokidar watcher is ready"));
 
-    // Load existing libraries and watch their paths
+    // Load the library paths from the database and start watching them
     this.initializeWatchedPaths();
 
     // Periodically refresh watched paths to catch any changes in the database
     setInterval(() => this.refreshWatchedPaths(), 1 * 60 * 1000); // every 1 minute
   }
 
+  // Helper methods to add/remove paths from the watcher and the internal map
+
+  private addPath(path: string, id: number) {
+    this.byPath.set(path, id);
+    this.watcher.add(path);
+  }
+
+  private removePath(path: string) {
+    this.watcher.unwatch(path);
+    this.byPath.delete(path);
+  }
+
+  /**
+   * Pulls the list of comic libraries from the database and adds their paths to the watcher if they are enabled.
+   */
   private async initializeWatchedPaths() {
     try {
       const libraries = await getAllComicLibraries();
@@ -67,33 +94,9 @@ export class WatchManager {
     }
   }
 
-  private async onFileAdded(path: string) {
-    // skip . files and hidden files
-    if (path.startsWith(".") || path.includes("/.")) {
-      return;
-    }
-    try {
-      await orchestrateFile(path);
-    } catch (error) {
-      apiLogger.error(
-        `Error calling orchestrateFile for ${path}: ${
-          error instanceof Error ? error.message : String(error)
-        }`,
-      );
-    }
-  }
-
-  private onFileRemoved(path: string) {
-    console.log(`File removed: ${path}`);
-    // TODO: Implement handling for file removals if needed
-    // Ideally it should be flagged as "missing" in the database and the data for the comic should be retained
-  }
-
-  private onFileChanged(path: string) {
-    console.log(`File changed: ${path}`);
-    // TODO: Implement handling for file changes if needed
-  }
-
+  /**
+   * Refreshes the list of watched paths by comparing the current state of the comic libraries in the database with the paths currently being watched.
+   */
   private async refreshWatchedPaths() {
     try {
       const libraries = await getAllComicLibraries();
@@ -132,16 +135,37 @@ export class WatchManager {
     }
   }
 
-  addPath(path: string, id: number) {
-    this.byPath.set(path, id);
-    this.watcher.add(path);
+  // The event handlers for chokidar events
+
+  private async onFileAdded(path: string) {
+    // skip . files and hidden files
+    if (path.startsWith(".") || path.includes("/.")) {
+      return;
+    }
+    try {
+      await orchestrateFile(path);
+    } catch (error) {
+      apiLogger.error(
+        `Error calling orchestrateFile for ${path}: ${
+          error instanceof Error ? error.message : String(error)
+        }`,
+      );
+    }
   }
 
-  removePath(path: string) {
-    this.watcher.unwatch(path);
-    this.byPath.delete(path);
+  private onFileRemoved(path: string) {
+    console.log(`File removed: ${path}`);
+    // TODO: Implement handling for file removals if needed
+    // Ideally it should be flagged as "missing" in the database and the data for the comic should be retained
   }
 
+  private onFileChanged(path: string) {
+    console.log(`File changed: ${path}`);
+    // TODO: Implement handling for file changes if needed
+  }
+
+  /*
+  TODO: Check if this is needed might be deprecated
   getChangedFiles(since: number): string[] {
     const changedFiles: string[] = [];
     for (const [path, timestamp] of this.byPath.entries()) {
@@ -151,6 +175,7 @@ export class WatchManager {
     }
     return changedFiles;
   }
+  */
 
   close() {
     this.watcher.close();
