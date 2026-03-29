0d930b518bb4845044b505bb81042225fd6d8df8
Alexis Montes <montes.alexis25@gmail.com>
2026-03-29 12:43:21 -0400

    updated the main queue file to import the config, and names

--- kitsume-komix-api/src/app/queues.ts
+++ kitsume-komix-api/src/app/queues.ts
@@ -8,27 +8,16 @@ import {
   workerEventFailed 
 } from "#infrastructure/queue/callbacks.ts";
 
-const defaultQueueOptions: QueueOptions = {
-  connection: redisConnection,
-  defaultJobOptions: {
-    removeOnComplete: true,
-    removeOnFail: false,
-    attempts: 3,
-    backoff: {
-      type: "exponential",
-      delay: 5000, // Initial delay of 5 seconds
-    },
-  },
-};
+import { defaultQueueOptions, queueNames } from "#config/queues.ts";
 
 // Define the main application queue for processing tasks
-export const appQueue = new Queue("appQueue", defaultQueueOptions);
+export const appQueue = new Queue(queueNames.APP_QUEUE, defaultQueueOptions);
 
 // Add connection event handlers
 appQueue.on("error", standardWorkerError);
 
 // Creates the event listeners for the main application queue: "appQueue"
-export const appQueueEvents = new QueueEvents("appQueue", {
+export const appQueueEvents = new QueueEvents(queueNames.APP_QUEUE, {
   connection: redisConnection,
 });
 
@@ -39,11 +28,11 @@ appQueueEvents.on("failed", workerEventFailed);
 appQueueEvents.on("error", standardWorkerError);
 
 
-export const fileOrchestrationQueue = new Queue("fileOrchestrationQueue", defaultQueueOptions);
+export const fileOrchestrationQueue = new Queue(queueNames.FILE_ORCHESTRATION_QUEUE, defaultQueueOptions);
 
 fileOrchestrationQueue.on("error", standardWorkerError);
   
-export const fileOrchestrationQueueEvents = new QueueEvents("fileOrchestrationQueue", {
+export const fileOrchestrationQueueEvents = new QueueEvents(queueNames.FILE_ORCHESTRATION_QUEUE, {
   connection: redisConnection,
 });
 
@@ -54,11 +43,11 @@ fileOrchestrationQueueEvents.on("failed", workerEventFailed);
 fileOrchestrationQueueEvents.on("error", standardWorkerError);
 
 
-export const fileQueue = new Queue("files", defaultQueueOptions);
+export const fileQueue = new Queue(queueNames.FILE_QUEUE, defaultQueueOptions);
 
 fileQueue.on("error", standardWorkerError);
 
-export const fileQueueEvents = new QueueEvents("files", {
+export const fileQueueEvents = new QueueEvents(queueNames.FILE_QUEUE, {
   connection: redisConnection,
 });
 
@@ -72,11 +61,11 @@ fileQueueEvents.on("error", standardWorkerError);
  * The Series Queue is responsible for processing series information and linking comic books to their respective series based on the parent folder structure 
  * It handles the creation of new series records in the database if they don't already exist, and ensures that comic books are properly associated with their series.
  */
-export const seriesQueue = new Queue("comicSeries", defaultQueueOptions);
+export const seriesQueue = new Queue(queueNames.COMIC_SERIES_QUEUE, defaultQueueOptions);
 
 seriesQueue.on("error", standardWorkerError);
 
-export const seriesQueueEvents = new QueueEvents("comicSeries", {
+export const seriesQueueEvents = new QueueEvents(queueNames.COMIC_SERIES_QUEUE, {
   connection: redisConnection,
 });
 
@@ -90,11 +79,11 @@ seriesQueueEvents.on("error", standardWorkerError);
  * The Comic Book Queue is responsible for the detailed processing of comic book files, including metadata extraction, thumbnail generation, and database insertion/updating.
  * It works closely with the file queue to receive jobs for new or updated comic files, and ensures that all necessary processing steps are completed before finalizing the comic book record in the database.
  */
-export const comicBookQueue = new Queue("comicBooks", defaultQueueOptions);
+export const comicBookQueue = new Queue(queueNames.COMIC_BOOK_QUEUE, defaultQueueOptions);
 
 comicBookQueue.on("error", standardWorkerError);
 
-export const comicBookQueueEvents = new QueueEvents("comicBooks", {
+export const comicBookQueueEvents = new QueueEvents(queueNames.COMIC_BOOK_QUEUE, {
   connection: redisConnection,
 });
 
