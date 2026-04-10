769d362c34af02059c37f716f2b3a1e18886bf04
Alexis Montes <montes.alexis25@gmail.com>
2026-04-10 14:33:35 -0400

    removed need of username as part of the user creation input

--- kitsume-komix-api/src/shared/types/user.type.ts
+++ kitsume-komix-api/src/shared/types/user.type.ts
@@ -1,5 +1,4 @@
 export type UserRegistrationInput = {
-  username: string;
   email: string;
   password: string; // plain text from client
   firstName?: string;
--- kitsume-komix-api/src/shared/zod/schemas/request.schema.ts
+++ kitsume-komix-api/src/shared/zod/schemas/request.schema.ts
@@ -242,11 +242,10 @@ export const ComicLibrarySchema = z.object({
 
 export const UserSchema = z.object({
   id: z.number().int().positive().optional(),
-  username: z.string().min(3).max(30),
   email: z.email(),
   password: z.string().min(8).max(100),
-  first_name: z.string().max(50).nullable().optional(),
-  last_name: z.string().max(50).nullable().optional(),
+  firstName: z.string().max(50).optional(),
+  lastName: z.string().max(50).optional(),
 });
 
 /**
