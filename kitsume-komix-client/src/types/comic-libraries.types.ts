042d908124e80575a795008db81437fc4480e97f
Alexis Montes <montes.alexis25@gmail.com>
2026-04-18 01:05:34 -0400

    Added additional types for the Create and Update data payload for the requests

--- kitsume-komix-client/src/types/comic-libraries.types.ts
+++ kitsume-komix-client/src/types/comic-libraries.types.ts
@@ -5,6 +5,18 @@ export type GetComicLibrariesResponse = paths['/comic-libraries']['get']['respon
 
 export type ComicLibrariesData = GetComicLibrariesResponse['libraries'];
 
+type CreateLibraryRequestBody = NonNullable<
+  paths['/comic-libraries/create-library']['post']['requestBody']
+>;
+
+export type CreateLibraryPayload = CreateLibraryRequestBody['content']['application/json'];
+
+type UpdateLibraryRequestBody = NonNullable<
+  paths['/comic-libraries/update-library/{id}']['post']['requestBody']
+>;
+
+export type UpdateLibraryPayload = UpdateLibraryRequestBody['content']['application/json'];
+
 // Library item - automatically extracted from the OpenAPI schema
 // Since data is now always an array, extraction is straightforward
 export type ComicLibrary = ComicLibrariesData extends (infer Item)[]
