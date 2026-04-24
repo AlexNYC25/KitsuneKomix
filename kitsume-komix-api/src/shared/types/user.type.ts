44635f0af67ef9cb22e5040224843e75cbd0ccd1
Alexis Montes <montes.alexis25@gmail.com>
2026-04-23 20:26:24 -0400

    Added a new user created for the input expected for updating a user

--- kitsume-komix-api/src/shared/types/user.type.ts
+++ kitsume-komix-api/src/shared/types/user.type.ts
@@ -1,6 +1,12 @@
+import type { z } from "zod";
+
+import type { EditUserRequestSchema } from "#zod/schemas/request.schema.ts";
+
 export type UserRegistrationInput = {
   email: string;
   password: string; // plain text from client
   firstName?: string;
   lastName?: string;
 };
+
+export type UserEditInput = z.infer<typeof EditUserRequestSchema>;
