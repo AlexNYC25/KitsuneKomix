f1439941e2353ec7b3586d008fabe0b91cd55728
Alexis Montes <montes.alexis25@gmail.com>
2026-04-26 12:05:35 -0400

    Added new icons + started the user settings components

--- kitsume-komix-client/src/components/Settings/UserSettings.vue
+++ kitsume-komix-client/src/components/Settings/UserSettings.vue
@@ -11,6 +11,15 @@
 	const users = computed(() => usersStore.getUsers);
 	const isAdmin = computed(() => Boolean(authStore.user?.admin));
 
+	const showEditUserForm = ref(false);
+	const showResetPasswordForm = ref(false);
+	const showEditUserLibrariesForm = ref(false);
+	const showDeleteUserConfirmationForm = ref(false)
+
+	const editedUserEmail = ref('')
+	const editedUserPassword = ref('')
+	const editedUserPasswordConfirmation = ref('')
+
 	const handleAddUser = () => {
 		// Logic to handle adding a user
 	};
@@ -73,25 +82,33 @@
 						class="grow-2 flex"
 					>
 						<div>
-							<button>
+							<button
+								@click="showEditUserForm = true"
+							>
 								<v-icon name="io-pencil-sharp" title="Edit User" scale="2" />
 							</button>
 						</div>
 
 						<div>
-							<button>
+							<button
+								@click="showResetPasswordForm = true"
+							>
 								<v-icon name="io-lock-closed" title="Edit Password" scale="2" />
 							</button>
 						</div>
 
 						<div>
-							<button>
-								<v-icon name="io-add-circle" title="Add Library" scale="2" />
+							<button
+								@click="showEditUserLibrariesForm = true"
+							>
+								<v-icon name="md-list-round" title="Add Library" scale="2" />
 							</button>
 						</div>
 
 						<div>
-							<button>
+							<button
+								@click="showDeleteUserConfirmationForm = true"
+							>
 								<v-icon name="io-trash" title="Delete User" scale="2" />
 							</button>
 						</div>
@@ -99,5 +116,86 @@
 				</div>
 			</div>
 		</div>
+
+		<!-- Edit User Form -->
+		<form
+			v-if="showEditUserForm && isAdmin"
+			class="lg:w-[520px] lg:h-[220px] flex flex-col fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 mb-6 shadow-2xl border border-gray-200 dark:border-gray-700 rounded-lg p-4 space-y-4 bg-white dark:bg-gray-900"
+			@submit.prevent=""
+		>
+			<h3 class="text-lg font-semibold">Edit User</h3>
+
+			<div>
+				<label for="library-name-edit" class="block text-sm font-medium mb-1">New Email</label>
+				<input
+					id="library-name-edit"
+					v-model="editedUserEmail"
+					type="text"
+					placeholder="e.g. user@example.com"
+					class="w-full px-3 py-2 border rounded-md bg-white dark:bg-gray-800 border-gray-300 dark:border-gray-700"
+				/>
+			</div>
+
+			<div class="flex items-center gap-2">
+				<Button
+					type="submit"
+					label="Save User"
+					severity="info"
+				/>
+
+				<Button
+					type="button"
+					label="Cancel"
+					severity="info"
+					@click="showEditUserForm = false"
+				/>
+			</div>
+		</form>
+
+		<!-- Change User Password Form -->
+		<form
+			v-if="showResetPasswordForm && isAdmin"
+			class="lg:w-[520px] lg:h-[420px] flex flex-col fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 mb-6 shadow-2xl border border-gray-200 dark:border-gray-700 rounded-lg p-4 space-y-4 bg-white dark:bg-gray-900"
+			@submit.prevent=""
+		>
+			<h3 class="text-lg font-semibold">Edit User's Password</h3>
+
+			<div>
+				<label for="library-name-edit" class="block text-sm font-medium mb-1">New Password</label>
+				<input
+					id="library-name-edit"
+					v-model="editedUserPassword"
+					type="password"
+					placeholder="Enter new password"
+					class="w-full px-3 py-2 border rounded-md bg-white dark:bg-gray-800 border-gray-300 dark:border-gray-700"
+				/>
+			</div>
+
+			<div>
+				<label for="library-name-edit" class="block text-sm font-medium mb-1">Confirm New Password</label>
+				<input
+					id="library-name-edit"
+					v-model="editedUserPasswordConfirmation"
+					type="password"
+					placeholder="Confirm new password"
+					class="w-full px-3 py-2 border rounded-md bg-white dark:bg-gray-800 border-gray-300 dark:border-gray-700"
+				/>
+			</div>
+
+			<div class="flex items-center gap-2">
+				<Button
+					type="submit"
+					label="Save Password"
+					severity="info"
+				/>
+
+				<Button
+					type="button"
+					label="Cancel"
+					severity="info"
+					@click="showResetPasswordForm = false"
+				/>
+			</div>
+		</form>
 	</div>
 </template>
