abd08ef12d4b54a93c9ce6f3a42568e643c19b8a
Alexis Montes <montes.alexis25@gmail.com>
2026-04-27 00:54:23 -0400

    Added a new user schema for representing info needed for registering a new user

---
+++ kitsume-komix-client/src/zod/users.schema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+import { toTypedSchema } from "@vee-validate/zod";
+
+export const userSignUpSchema = toTypedSchema(z.object({
+	userEmail: z.email('Invalid email address'),
+	userRole: z.union([z.literal('user'), z.literal('admin')]),
+	userPassword: z.string().min(6, 'Password must be at least 6 characters long')
+}))
