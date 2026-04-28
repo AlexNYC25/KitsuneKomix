226d62ac34818e6631f5c10a225667d6155955a3
Alexis Montes <montes.alexis25@gmail.com>
2026-04-27 22:19:42 -0400

    Generated new types for registering a user from the openapi schema

--- kitsume-komix-client/src/types/users.types.ts
+++ kitsume-komix-client/src/types/users.types.ts
@@ -3,3 +3,9 @@ import type { paths } from "@/openapi/openapi-schema";
 export type GetUsersResponse = paths["/users"]["get"]["responses"]["200"]["content"]["application/json"];
 
 export type User = GetUsersResponse["users"][number];
+
+export type UserRegistrationRequestBody = NonNullable<
+  paths['/users/create-user']['post']['requestBody']
+>;
+
+export type UserRegistractionPayload = UserRegistrationRequestBody['content']['application/json']
