40140b98816f65762fc7c1f1af762da758a8d92d
Alexis Montes <montes.alexis25@gmail.com>
2026-04-28 00:47:45 -0400

    Add createInsertSchema to schema factory exports

--- kitsume-komix-api/src/shared/zod/factory.ts
+++ kitsume-komix-api/src/shared/zod/factory.ts
@@ -1,4 +1,5 @@
 import { z } from "@hono/zod-openapi";
 import { createSchemaFactory } from "drizzle-zod";
 
-export const { createSelectSchema } = createSchemaFactory({ zodInstance: z });
+export const { createSelectSchema, createInsertSchema } = createSchemaFactory({ zodInstance: z });
+
