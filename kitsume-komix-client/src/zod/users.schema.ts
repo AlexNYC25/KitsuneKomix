950778c06b71d3d6f783ffced99ba8047505516b
Alexis Montes <montes.alexis25@gmail.com>
2026-05-02 19:12:20 -0400

    Added password check schema

--- kitsume-komix-client/src/zod/users.schema.ts
+++ kitsume-komix-client/src/zod/users.schema.ts
@@ -11,3 +11,13 @@ export const userInfoEditSchema = toTypedSchema(z.object({
 	userEmail: z.email('Invalid email address'),
 	userRole: z.union([z.literal('user'), z.literal('admin')]).optional(),
 }))
+
+export const userPasswordEditSchema = toTypedSchema(
+	z.object({
+		newPassword: z.string().min(6, 'Password must be at least 6 characters long'),
+		confirmPassword: z.string().min(6, 'Password must be at least 6 characters long'),
+	}).refine((values) => values.newPassword === values.confirmPassword, {
+		message: 'Passwords do not match',
+		path: ['confirmPassword'],
+	}),
+)
