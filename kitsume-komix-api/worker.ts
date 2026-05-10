b8b556153bf054aedbd942ff0c94a136d86801e4
Alexis Montes <montes.alexis25@gmail.com>
2026-05-10 00:00:22 -0400

    Seperate the background worker app to its own seperate process

--- kitsume-komix-api/worker.ts
+++ kitsume-komix-api/worker.ts
@@ -1,13 +1,14 @@
-import app from "#app/api.ts";
-import { startWatcher } from "#app/watcher.ts";
-
 import { env } from "#config/env.ts";
 
 import { runMigrations } from "#infrastructure/db/scripts/migrate.ts";
 import { setUpAppSettings } from "#infrastructure/db/scripts/seed.ts";
 
+import "#infrastructure/queue/workers/comicBook.worker.ts";
+import "#infrastructure/queue/workers/series.worker.ts";
+import "#infrastructure/queue/workers/file.worker.ts";
+
 await runMigrations();
 await setUpAppSettings();
-await startWatcher();
 
-Deno.serve({ port: env.PORT }, app.fetch);
+// Keep the process alive
+await new Promise(() => {});
