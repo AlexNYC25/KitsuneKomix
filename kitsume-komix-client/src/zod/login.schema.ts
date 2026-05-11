9811e83b0815c06211f57d40b1c236d5316c67ab
Alexis Montes <montes.alexis25@gmail.com>
2026-05-10 21:12:57 -0400

    Updated zod email statements to be consistant with the latest version's remomendations

--- kitsume-komix-client/src/zod/login.schema.ts
+++ kitsume-komix-client/src/zod/login.schema.ts
@@ -3,7 +3,7 @@ import { toTypedSchema } from '@vee-validate/zod';
 
 export const validateEmailPassword = toTypedSchema(
   z.object({
-    email: z.string().email({ message: "Invalid email address" }),
-    password: z.string().min(6, { message: "Password must be at least 6 characters long" })
+    email: z.email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters long')
   })
 );
--- kitsume-komix-client/src/zod/signup.schema.ts
+++ kitsume-komix-client/src/zod/signup.schema.ts
@@ -3,9 +3,9 @@ import { toTypedSchema } from '@vee-validate/zod';
 
 export const validateSignup = toTypedSchema(
   z.object({
-    email: z.string().email({ message: "Invalid email address" }),
-    password: z.string().min(6, { message: "Password must be at least 6 characters long" }),
-    confirmPassword: z.string().min(6, { message: "Confirm password must be at least 6 characters long" })
+    email: z.email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters long'),
+    confirmPassword: z.string().min(6, 'Confirm password must be at least 6 characters long')
   }).refine((data) => data.password === data.confirmPassword, {
     message: "Passwords do not match",
     path: ["confirmPassword"],
