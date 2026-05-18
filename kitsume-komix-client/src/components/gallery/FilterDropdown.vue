afd4fb58da8a020f5a43ab5c9c5b1dde4dc3b228
Alexis Montes <montes.alexis25@gmail.com>
2026-05-18 14:34:30 -0400

    Added a new Component to show the dropdown options

---
+++ kitsume-komix-client/src/components/gallery/FilterDropdown.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import AppIcon from '../icons/AppIcon.vue';
+
+defineProps<{
+	property: {
+		id: number | string
+		name: string
+	},
+	isSelected?: boolean
+}>();
+
+const emit = defineEmits<{
+	toggle: []
+	untoggle: []
+}>();
+
+const handleClick = (isSelected: boolean | undefined) => {
+	if (isSelected) {
+		emit('untoggle');
+	} else {
+		emit('toggle');
+	}
+};
+</script>
+
+<template>
+	<label
+		class="flex items-center gap-2 px-3 py-1.5 hover:bg-white/5 cursor-pointer text-sm text-text-primary"
+	>
+		<button
+			type="button"
+			@click.prevent="handleClick(isSelected)"
+			class="p-0 border-0 bg-transparent cursor-pointer"
+		>
+			<AppIcon
+				:name="isSelected ? 'remove' : 'add'"
+				scale="0.8"
+				:class="isSelected ? 'text-red-400' : 'text-green-400'"
+			/>
+		</button>
+		{{ property.name }}
+	</label>
+</template>
