d2f0c88c1ac601dbc9e3fb98d1f0b1f1e439642c
Alexis Montes <montes.alexis25@gmail.com>
2026-05-21 01:29:34 -0400

    Added a wrapper component that creates the grid that shows the comic series

---
+++ kitsume-komix-client/src/components/gallery/GalleryResultsGrid.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import ComicSeriesCard from '../ComicSeriesCard.vue';
+
+import type { ComicSeriesListItem } from '@/types';
+
+defineProps<{
+  comics: ComicSeriesListItem[];
+}>();
+</script>
+
+<template>
+  <div class="grid grid-cols-5 gap-6">
+    <ComicSeriesCard
+      v-for="comic in comics"
+      :key="comic.id"
+      :comicSeriesData="comic"
+    />
+  </div>
+</template>
