1ec79f82494a826179261d7c2b0fdff81138bc4d
Alexis Montes <montes.alexis25@gmail.com>
2026-05-23 01:30:59 -0400

    Updated the imports to use the type import decleration

--- kitsume-komix-api/src/shared/types/responses.type.ts
+++ kitsume-komix-api/src/shared/types/responses.type.ts
@@ -1,5 +1,5 @@
-import { z } from "zod";
-import {
+import type { z } from "zod";
+import type {
   ComicBookMultipleResponseSchema,
   ComicSeriesMultipleResponseSchema,
 } from "#zod/schemas/response.schema.ts";
