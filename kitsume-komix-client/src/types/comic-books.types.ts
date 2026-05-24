7095a7e4338b585e35080627d01dfec66234f93e
Alexis Montes <montes.alexis25@gmail.com>
2026-05-24 02:37:05 -0400

    Generated new types from the comic-books/filter-values route

--- kitsume-komix-client/src/types/comic-books.types.ts
+++ kitsume-komix-client/src/types/comic-books.types.ts
@@ -33,3 +33,42 @@ export type GetComicBookThumbnailsResponse = paths['/comic-books/{id}/thumbnails
 export type ComicBookThumbnailsData = GetComicBookThumbnailsResponse['thumbnails'];
 
 export type ComicBookThumbnail = ComicBookThumbnailsData[number];
+
+// /comic-books/filter-values
+export type GetComicBooksFilterValuesResponse = paths['/comic-books/filter-values']['get']['responses']['200']['content']['application/json'];
+
+export type ComicBooksFilterValuesData = GetComicBooksFilterValuesResponse['data'];
+
+export type ComicBookWriterValues = NonNullable<ComicBooksFilterValuesData['writers']>[number];
+
+export type ComicBookPencillerValues = NonNullable<ComicBooksFilterValuesData['pencillers']>[number];
+
+export type ComicBookInkerValues = NonNullable<ComicBooksFilterValuesData['inkers']>[number];
+
+export type ComicBookColoristValues = NonNullable<ComicBooksFilterValuesData['colorists']>[number];
+
+export type ComicBookLettererValues = NonNullable<ComicBooksFilterValuesData['letterers']>[number];
+
+export type ComicBookEditorValues = NonNullable<ComicBooksFilterValuesData['editors']>[number];
+
+export type ComicBookCoverArtistValues = NonNullable<ComicBooksFilterValuesData['coverArtists']>[number];
+
+export type ComicBookPublisherValues = NonNullable<ComicBooksFilterValuesData['publishers']>[number];
+
+export type ComicBookImprintValues = NonNullable<ComicBooksFilterValuesData['imprints']>[number];
+
+export type ComicBookGenreValues = NonNullable<ComicBooksFilterValuesData['genres']>[number];
+
+export type ComicBookCharacterValues = NonNullable<ComicBooksFilterValuesData['characters']>[number];
+
+export type ComicBookTeamValues = NonNullable<ComicBooksFilterValuesData['teams']>[number];
+
+export type ComicBookLocationValues = NonNullable<ComicBooksFilterValuesData['locations']>[number];
+
+export type ComicBookStoryArcValues = NonNullable<ComicBooksFilterValuesData['storyArcs']>[number];
+
+export type ComicBookSeriesGroupValues = NonNullable<ComicBooksFilterValuesData['seriesGroups']>[number];
+
+export type ComicBookYearValues = NonNullable<ComicBooksFilterValuesData['years']>[number];
+
+export type ComicBookLetterValues = NonNullable<ComicBooksFilterValuesData['letters']>[number];
