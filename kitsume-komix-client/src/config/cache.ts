3f6ce9ffe1c275233b2a09c26a9354980a0e2874
Alexis Montes <montes.alexis25@gmail.com>
2026-05-25 16:45:18 -0400

    Added typing to the hard coded cache ttl value

--- kitsume-komix-client/src/config/cache.ts
+++ kitsume-komix-client/src/config/cache.ts
@@ -1,3 +1,3 @@
 
 // The time to live for cached comic store entries, in milliseconds
-export const CACHE_TTL_MS = 10 * 60 * 1000; // 10 minutes
+export const CACHE_TTL_MS: number = 10 * 60 * 1000; // 10 minutes
