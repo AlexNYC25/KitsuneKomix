19db896e2908305cb15bab0f9710a92d5b06b959
Alexis Montes <montes.alexis25@gmail.com>
2026-06-16 23:23:22 -0400

    Added additional scaffolding

--- kitsume-komix-api/src/modules/workers/handlers/comic-metadata-aggregated.handler.ts
+++ kitsume-komix-api/src/modules/workers/handlers/comic-metadata-aggregated.handler.ts
@@ -25,7 +25,59 @@ export class ComicMetadataAggregatedHandler implements JobHandler {
       }
 
       for (const candidate of metadataCandidatesToProcess) {
-
+        switch (candidate.type) {
+          case "genre":
+            // 
+            break;
+          case "publisher":
+            // 
+            break;
+          case "imprint":
+            // 
+            break;
+          case "writer":
+            // 
+            break;
+          case "penciler":
+            // 
+            break;
+          case "inker":
+            // 
+            break;
+          case "colorist":
+            //
+            break;
+          case "letterer":
+            //
+            break;
+          case "cover_artist":
+            //
+            break;
+          case "editor":
+            //
+            break;
+          case "character":
+            //
+            break;
+          case "team":
+            //
+            break;
+          case "location":
+            //
+            break;
+          case "story_arc":
+            //
+            break;
+          case "series_group":
+            //
+            break;
+          case "series":
+            continue;
+          default:
+            queueLogger.warn(
+              `[ComicMetadataAggregatedHandler] Unrecognized candidate type: ${candidate.type} for candidate ID ${candidate.id}`,
+            );
+        }
       }
 
       const newStateRecord: Partial<ComicBookIngestion> = {
@@ -57,4 +109,49 @@ export class ComicMetadataAggregatedHandler implements JobHandler {
       };
     }
   }
+}
+
+const aggregateGenreMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregatePublisherMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregateImprintMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregateWriterMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregatePencilerMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregateInkerMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregateColoristMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregateLettererMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregateCoverArtistMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregateEditorMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregateCharacterMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregateTeamMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregateLocationMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregateStoryArcMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
+}
+
+const aggregateSeriesGroupMetadata = async (candidate: ComicMetadataCandidate): Promise<void> => {
 }
