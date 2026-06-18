e8284a642acfd46a11e259fb4d5e94f598401596
Alexis Montes <montes.alexis25@gmail.com>
2026-06-17 20:20:43 -0400

    removed new worker implementation

--- kitsume-komix-api/src/modules/workers/handlers/index.ts
+++
@@ -1,6 +0,0 @@
-export { FileDetectedHandler } from "./file-detected.handler.ts";
-export { MetadataExtractionHandler } from "./metadata-extraction.handler.ts";
-export { MetadataCandidatesCreationHandler } from "./metadata-candidates-creation.handler.ts";
-export { MetadataEntitiesResolutionHandler } from "./metadata-entities-resolution.handler.ts";
-export { ComicLinksBuilderHandler } from "./comic-links-builder.handler.ts";
-export { ComicMetadataAggregatedHandler } from "./comic-metadata-aggregated.handler.ts";
