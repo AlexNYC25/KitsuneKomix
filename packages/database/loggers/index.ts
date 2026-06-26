7c608c9ee7d8c0bb07f772fb9f3867f30bcbdea9
Alexis Montes <montes.alexis25@gmail.com>
2026-06-26 00:55:54 -0400

    Added some initial logic to create the logs directory and db file when they don't exist yet

--- packages/database/loggers/index.ts
+++ packages/database/loggers/index.ts
@@ -5,6 +5,11 @@ import { env } from "../config/env.ts";
 
 const configLocation = env.CONFIG_DIRECTORY;
 const logsDir = join(configLocation, "logs");
+const dbLogFile = join(logsDir, "db.log")
+
+await Deno.mkdir(logsDir, { recursive: true });
+const file = await Deno.open(dbLogFile, { create: true, write: true });
+file.close();
 
 export const initDbLogger = async (): Promise<void> => {
   await Deno.mkdir(logsDir, { recursive: true });
@@ -12,4 +17,4 @@ export const initDbLogger = async (): Promise<void> => {
 
 export const dbLogger = pino({
   level: env.LOG_LEVEL,
-}, pino.destination(join(logsDir, "db.log")));
+}, pino.destination(dbLogFile));
