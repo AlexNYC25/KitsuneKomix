7d20d6c5cbab4657b86c481f65564ac4e1b908a6
Alexis Montes <montes.alexis25@gmail.com>
2026-06-25 22:00:35 -0400

    added additional log level for the env schema

--- packages/database/shared/schemas/db.schema.ts
+++ packages/database/shared/schemas/db.schema.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 
 export const dbSchema = z.object({
-  CONFIG_DIRECTORY: z.string().default("/app/data/config")
+  CONFIG_DIRECTORY: z.string().default("/app/data/config"),
+
+  LOG_LEVEL: z.string().default("info")
 })
