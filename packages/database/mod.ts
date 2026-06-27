8eedc27e2a2e7c9da8e472ff1e1b5b24ae7d2654
Alexis Montes <montes.alexis25@gmail.com>
2026-06-27 14:37:51 -0400

    exported additional functions

--- packages/database/mod.ts
+++ packages/database/mod.ts
@@ -1,2 +1,7 @@
 export * from "./client.ts"
+export * from "./honker/queue.ts"
+
+export * from "./config/env.ts"
+export * from "./utilities/db-file.ts"
+
 export * from "./setup/install-honker.ts"
