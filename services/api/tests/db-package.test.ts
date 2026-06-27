c2f4a653c45b449a448a4206b7ce219a295623b2
Alexis Montes <montes.alexis25@gmail.com>
2026-06-27 14:38:08 -0400

    added additional db related tests

--- services/api/tests/db-package.test.ts
+++ services/api/tests/db-package.test.ts
@@ -1,9 +1,21 @@
 import { assertEquals } from "@std/assert/equals";
 
-import { testSQLiteConnection } from "kitsune-komix-database";
+import { testSQLiteConnection, generateSqlFilePath, env } from "kitsune-komix-database";
 
 Deno.test("Database package can be pinged", async () => {
   const sqliteConnectionWorks: boolean = await testSQLiteConnection();
 
   assertEquals(sqliteConnectionWorks, true, "Something is wrong with the db connection set up and cannot be pinged with a select test")
 })
+
+Deno.test("Database File exists", async () => {
+  const sqlitePath = await generateSqlFilePath(env.CONFIG_DIRECTORY);
+
+  try {
+    await Deno.lstat(sqlitePath);
+  } catch (error) {
+    if (error instanceof Deno.errors.NotFound) {
+      await Deno.writeTextFile(sqlitePath, "")
+    }
+  }
+}) 
--- services/api/tests/honker-ext.test.ts
+++ services/api/tests/honker-ext.test.ts
@@ -1,10 +1,26 @@
 import { assertEquals } from "@std/assert"
 import { exists } from "@std/fs/exists"
 
+import { enqueue } from "kitsune-komix-database";
+
 Deno.test("Extension exists", async () => {
   const path: string = "/honker/libhonker_ext.so"
 
   const doesTheBinaryExtensionFileExist: boolean = await exists(path)
 
   assertEquals(doesTheBinaryExtensionFileExist, true, "The Honker Extension file does not exist, check the dockerfile/local build")
+})
+
+Deno.test("Honker enqueue action works", async () => {
+  const queueOptions = {
+    name: "test",
+    maxAttempts: 3
+  }
+
+  const queueItemOptions = {
+    delay: undefined,
+    priority: undefined
+  }
+  
+  enqueue(queueOptions, queueItemOptions, "TEST")
 })
