d0e239ebf0efad6c2ff76ae87c657464e9baac50
Alexis Montes <montes.alexis25@gmail.com>
2026-06-29 02:15:12 -0400

    Updated the install honker script to use the safer prepare function instead of the direct client execute function

--- packages/database/setup/install-honker.ts
+++ packages/database/setup/install-honker.ts
@@ -2,5 +2,5 @@ import type Database from 'better-sqlite3';
 
 export const installHonkerExtension = (sqliteDatabase: ReturnType<typeof Database>) => {
   sqliteDatabase.loadExtension('/honker/libhonker_ext.so');
-  sqliteDatabase.exec('SELECT honker_bootstrap()');
+  sqliteDatabase.prepare('SELECT honker_bootstrap()').run();
 };
