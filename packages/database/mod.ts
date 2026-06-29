828c86faebbd78e1a84a12b5916f24d9bd4da5b9
Alexis Montes <montes.alexis25@gmail.com>
2026-06-29 15:26:03 -0400

    Removed the install-honker script as no longer needed with the bun sqlite package

--- packages/database/mod.ts
+++ packages/database/mod.ts
@@ -3,5 +3,3 @@ export * from "./honker/queue.ts"
 
 export * from "./config/env.ts"
 export * from "./utilities/db-file.ts"
-
-export * from "./setup/install-honker.ts"
--- packages/database/setup/install-honker.ts
+++
@@ -1,6 +0,0 @@
-import type Database from 'better-sqlite3';
-
-export const installHonkerExtension = (sqliteDatabase: ReturnType<typeof Database>) => {
-  sqliteDatabase.loadExtension('/honker/libhonker_ext.so');
-  sqliteDatabase.prepare('SELECT honker_bootstrap()').run();
-};
