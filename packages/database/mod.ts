b4b43c59b4bc96e3f26c356dd4f15580e6caf03d
Alexis Montes <montes.alexis25@gmail.com>
2026-07-01 22:59:58 -0400

    Updated the general mod export file

--- packages/database/mod.ts
+++ packages/database/mod.ts
@@ -1,5 +1,5 @@
-export * from "./client.ts"
-export * from "./honker/queue.ts"
+export * from "./drizzle/client.ts"
+export * from "./honker/client.ts"
 
 export * from "./config/env.ts"
 export * from "./utilities/db-file.ts"
