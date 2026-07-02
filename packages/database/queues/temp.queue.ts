a02435e1c98b7d8329b7f3c068f84817e875b0b7
Alexis Montes <montes.alexis25@gmail.com>
2026-07-01 23:06:15 -0400

    added a temp queue for temporary payloads for testing

---
+++ packages/database/queues/temp.queue.ts
@@ -0,0 +1,16 @@
+import { open } from "@russellthehippo/honker-bun"
+
+import { env } from "../config/env.ts"
+import { queueLogger } from "../loggers/index.ts";
+import { generateSqlFilePath } from "../utilities/db-file.ts"
+
+export const tempQueue = async () => {
+  const sqlitePath: string = await generateSqlFilePath(env.CONFIG_DIRECTORY);
+  const honkerPath: string = "/honker/libhonker_ext.so"
+
+  const db = open(sqlitePath, honkerPath);
+
+  const q = db.queue("temp");
+
+  return q;
+}
