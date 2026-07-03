d0f24e8c4dedbbc9f425111c7dccb7a09b85fb16
Alexis Montes <montes.alexis25@gmail.com>
2026-07-03 17:38:17 -0400

    set up initial watcher index.ts file

--- services/watcher/index.ts
+++ services/watcher/index.ts
@@ -1,3 +1,11 @@
+import { getQueueClient } from "kitsune-komix-database"
+
 import { WatchManager } from "./watcher/manager";
 
 const workerWatcher = new WatchManager();
+
+const queueClient = await getQueueClient();
+
+for await (const notif of queueClient.listen("watcher")) {
+  console.log(notif.channel, notif.payload);
+}
