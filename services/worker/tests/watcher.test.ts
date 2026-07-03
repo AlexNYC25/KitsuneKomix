c20b727f309f0031aa7e8eb1726dace90e7825d8
Alexis Montes <montes.alexis25@gmail.com>
2026-07-03 01:20:27 -0400

    added skeleton of regression testing to preform

---
+++ services/worker/tests/watcher.test.ts
@@ -0,0 +1,33 @@
+import { expect, test } from "bun:test"
+
+test("file added -> enqueued to added queue", () => {
+
+})
+
+test("file modified -> enqueued to modified queue", () => {
+
+})
+
+test("file removed -> remove job", () => {
+
+})
+
+test("file removed -> mark file deleted", () => {
+
+})
+
+test("new directory added dynamically", () => {
+
+})
+
+test("removed directory stops receiving events", () => {
+
+})
+
+test("nested directories are watched", () => {
+
+})
+
+test("ignoreinitial works", () => {
+
+})
