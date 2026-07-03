84e85d0e23d0db4a9e346c54e5d93735dceab0e2
Alexis Montes <montes.alexis25@gmail.com>
2026-07-03 01:39:24 -0400

    fixed queue client function to return the actual db queue object

--- packages/database/honker/client.ts
+++ packages/database/honker/client.ts
@@ -3,12 +3,16 @@ import { open } from "@russellthehippo/honker-bun";
 import { env } from "../config/env.ts"
 import { generateSqlFilePath } from "../utilities/db-file.ts"
 
-const queue: ReturnType<typeof open> | null = null
+let queue: ReturnType<typeof open> | null = null
 
 export const getQueueClient = async () => {
   if(!queue) {
     const sqlitePath: string = await generateSqlFilePath(env.CONFIG_DIRECTORY);
 
     const db = open(sqlitePath, "/honker/libhonker_ext.so")
+
+    queue = db;
   }
+
+  return queue;
 }
