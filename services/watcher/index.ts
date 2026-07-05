2d65650c44b1f6cf82b524f81e4bfb58ccae9ea4
Alexis Montes <montes.alexis25@gmail.com>
2026-07-05 00:35:22 -0400

    removed pub/sub usage

--- services/watcher/index.ts
+++ services/watcher/index.ts
@@ -1,11 +1,3 @@
-import { getQueueClient } from "kitsune-komix-database"
-
 import { WatchManager } from "./watcher/manager";
 
 const workerWatcher = new WatchManager();
-
-const queueClient = await getQueueClient();
-
-for await (const notif of queueClient.listen("watcher")) {
-  console.log(notif.channel, notif.payload);
-}
