a77d87e0cc4621c5a618bf05162cf987dbea92d4
Alexis Montes <montes.alexis25@gmail.com>
2026-07-05 02:20:53 -0400

    Updated the db schema to also accept an alternative location for the honker lib file with a default value

--- packages/database/shared/schemas/db.schema.ts
+++ packages/database/shared/schemas/db.schema.ts
@@ -3,5 +3,7 @@ import { z } from "zod";
 export const dbSchema = z.object({
   CONFIG_DIRECTORY: z.string().default("/app/data/config"),
 
+  HONKER_LIB_PATH: z.string().default("/honker/libhonker_ext.so"),
+
   LOG_LEVEL: z.string().default("info")
 })
