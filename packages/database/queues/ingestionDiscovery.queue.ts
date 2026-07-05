71a2cd164d6bc3e553e452ad5858fdb9106fdd7e
Alexis Montes <montes.alexis25@gmail.com>
2026-07-05 02:19:47 -0400

    added additional queues for the chokidar callbacks

---
+++ packages/database/queues/ingestionDiscovery.queue.ts
@@ -0,0 +1,16 @@
+import { open } from "@russellthehippo/honker-bun"
+
+import { env } from "../config/env.ts"
+import { QueueNames } from "../config/queues.ts"
+import { generateSqlFilePath } from "../utilities/db-file.ts"
+
+export const getIngestionDiscoveryQueue = async () => {
+  const sqlitePath: string = await generateSqlFilePath(env.CONFIG_DIRECTORY);
+  const honkerPath: string = env.HONKER_LIB_PATH
+
+  const db = open(sqlitePath, honkerPath);
+
+  const q = db.queue(QueueNames.INGESTION_DISCOVERY);
+
+  return q;
+}
