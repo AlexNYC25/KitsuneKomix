d9c4727bfcd582200c10815b1ba49404b825c015
Alexis Montes <montes.alexis25@gmail.com>
2026-07-05 02:21:34 -0400

    defined some of the types that would be encountered when dealing with the queues and export them in the mod.ts file

--- packages/database/mod.ts
+++ packages/database/mod.ts
@@ -2,6 +2,13 @@ export * from "./drizzle/client.ts"
 export * from "./honker/client.ts"
 
 export * from "./queues/temp.queue.ts"
+export * from "./queues/ingestionDiscovery.queue.ts"
+export * from "./queues/fileModified.queue.ts"
+export * from "./queues/fileRemoved.queue.ts"
 
 export * from "./config/env.ts"
+export * from "./config/queues.ts"
+
 export * from "./utilities/db-file.ts"
+
+export * from "./shared/types/queue.type.ts"
---
+++ packages/database/shared/types/queue.type.ts
@@ -0,0 +1,5 @@
+import type { Queue, Job} from "@russellthehippo/honker-bun"
+
+export type QueueType = Queue;
+
+export type QueueJob = Job;
