ba40a5d3504f2e05213ba90bc9fc95ceaf1a1e4e
Alexis Montes <montes.alexis25@gmail.com>
2026-07-06 01:13:30 -0400

    set up basic ingestion worker that loads a job from the queue and right now does nothing with it yet

---
+++ services/worker/workers/ingestionWorker.ts
@@ -0,0 +1,37 @@
+import { 
+  getIngestionDiscoveryQueue, 
+  type QueueJob, 
+  type QueueType 
+} from "kitsune-komix-database"
+
+export class IngestionWorker {
+  queue: null | QueueType = null;
+
+  async dequeue() {
+    if (!this.queue) {
+      this.queue = await getIngestionDiscoveryQueue();
+    }
+
+    const job: QueueJob | null = this.queue.claimOne("ingestion_worker");
+
+    return job;
+  }
+    
+  async start() {
+    while (true) {
+      const job: QueueJob | null = await this.dequeue();
+
+      if (!job) {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        continue;
+      }
+
+      await this.processJob(job);
+    }
+  }
+
+  async processJob(job: any) {
+
+  }
+}
