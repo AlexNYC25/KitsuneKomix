8785e17dfa975ef91a976a1050e43624bdf6de75
Alexis Montes <montes.alexis25@gmail.com>
2026-07-05 23:44:13 -0400

    Updated the watch manager that has the additional queues and pushes a value to the respective queue based on the action callback

--- services/watcher/watcher/manager.ts
+++ services/watcher/watcher/manager.ts
@@ -1,5 +1,12 @@
 import chokidar from "chokidar"
 
+import { 
+  getIngestionDiscoveryQueue,
+  getFileModifiedQueue,
+  getFileRemovedQueue,
+  type QueueType
+} from "kitsune-komix-database"
+
 export class WatchManager {
   private watcher = chokidar.watch([], {
     persistent:true,
@@ -10,6 +17,10 @@ export class WatchManager {
     }
   });
 
+  private discoveryQueue: null | QueueType = null;
+  private fileModifiedQueue: null | QueueType = null;
+  private fileRemovedQueue: null | QueueType = null;
+
   private watched = new Set<string>();
 
   constructor() {
@@ -18,24 +29,42 @@ export class WatchManager {
     this.watcher.on("unlink", this.onFileRemoved)
   }
 
-  private onFileAdded = (path: string) => {
+  private onFileAdded = async (path: string) => {
     console.log("new file", path)
+    
+    if (!this.discoveryQueue) {
+      this.discoveryQueue = await getIngestionDiscoveryQueue();
+    }
+
+    this.discoveryQueue.enqueue({ path })
   }
 
-  private onFileChanged = (path: string) => {
+  private onFileChanged = async (path: string) => {
     console.log("file changed", path)
+
+    if (!this.fileModifiedQueue) {
+      this.fileModifiedQueue = await getFileModifiedQueue();
+    }
+
+    this.fileModifiedQueue.enqueue({ path })
   }
 
-  private onFileRemoved = (path: string) => {
+  private onFileRemoved = async (path: string) => {
     console.log("removed", path)
+
+    if (!this.fileRemovedQueue) {
+      this.fileRemovedQueue = await getFileRemovedQueue();
+    }
+
+    this.fileRemovedQueue.enqueue({ path })
   }
 
   async syncDirectories(nextDirectories: string []) {
-    const next = new Set(nextDirectories);
+    const next: Set<string> = new Set(nextDirectories);
 
-    const toAdd = [...next].filter(dir => !this.watched.has(dir))
+    const toAdd: string[] = [...next].filter(dir => !this.watched.has(dir))
 
-    const toRemove = [...next].filter(dir => !next.has(dir))
+    const toRemove: string[] = [...next].filter(dir => !next.has(dir))
 
     if (toAdd.length) {
       this.watcher.add(toAdd)
