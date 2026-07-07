17c2ec524434435b103e6f452f88827fe3651d3a
Alexis Montes <montes.alexis25@gmail.com>
2026-07-07 01:17:46 -0400

    removed unneeded column

--- packages/database/schemas/tables/comicLibraries.table.ts
+++ packages/database/schemas/tables/comicLibraries.table.ts
@@ -1,14 +1,12 @@
 import { int, snakeCase, text } from "drizzle-orm/sqlite-core";
 import { sql } from "drizzle-orm/sql";
 
-// TODO: check if the changedAt field is unnecessary since we have the updatedAt
 export const comicLibrariesTable = snakeCase.table("comic_libraries", {
   id: int().primaryKey({ autoIncrement: true }),
   name: text().notNull(),
   path: text().notNull().unique(),
   description: text(),
   enabled: int({mode: "boolean"}).notNull().default(true),
-  changedAt: text().notNull().default(sql`CURRENT_TIMESTAMP`),
   createdAt: text().notNull().default(sql`CURRENT_TIMESTAMP`),
   updatedAt: text().notNull().default(sql`CURRENT_TIMESTAMP`),
 });
