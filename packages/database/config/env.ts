e367285785d93c50ef448012b5b34286eb71b0f1
Alexis Montes <montes.alexis25@gmail.com>
2026-07-09 16:14:36 -0400

    moved env schema locally

--- packages/database/config/env.ts
+++ packages/database/config/env.ts
@@ -1,3 +1,12 @@
-import { dbSchema } from "@schemas/db.schema.ts"
+import { z } from "zod";
+
+// NOTE: declared this schema locally as there is a compatibility issue between folder aliasas and drizzle-kit
+const dbSchema = z.object({
+  CONFIG_DIRECTORY: z.string().default("/app/data/config"),
+  HONKER_LIB_PATH: z.string().default("/honker/libhonker_ext.so"),
+  LOG_LEVEL: z.string().default("info"),
+  PAGE_SIZE: z.number().default(20),
+  DB_FILE_NAME: z.string().default("database.sqlite"),
+});
 
 export const env = dbSchema.parse(process.env);
