602076dc09ceccbbf1d3e8df98f4be008dae3952
Alexis Montes <montes.alexis25@gmail.com>
2026-07-09 12:28:09 -0400

    added the scripts using the mod export file

--- packages/database/mod.ts
+++ packages/database/mod.ts
@@ -11,4 +11,8 @@ export * from "./config/queues.ts"
 
 export * from "./utilities/db-file.ts"
 
+export * from "./scripts/dbml.ts"
+export * from "./scripts/migrate.ts"
+export * from "./scripts/seed.ts"
+
 export * from "./shared/types/queue.type.ts"
