660727ebbd3c1a96bd051743a3055f477b1f6eb8
Alexis Montes <montes.alexis25@gmail.com>
2026-07-09 20:35:01 -0400

    Updated the order of calling the queue + drizzle client to initialize both the honker and drizzle tables

--- services/api/main.ts
+++ services/api/main.ts
@@ -1,4 +1,5 @@
-import { getClient, runMigrations } from "kitsune-komix-database";
+import { getClient, getQueueClient, runMigrations } from "kitsune-komix-database";
+
 const server = Bun.serve({
   port: 8001,
   routes: {
@@ -6,9 +7,10 @@ const server = Bun.serve({
   }
 });
 
-const dbClient = await getClient();
+const honkerDb = await getQueueClient();
 
 await runMigrations()
 
+const dbClient = await getClient();
 
 console.log(`Listening on ${server.url}`);
