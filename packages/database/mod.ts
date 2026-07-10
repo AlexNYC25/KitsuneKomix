01a81afa98feec3e36345a469a71ed3653bb031f
Alexis Montes <montes.alexis25@gmail.com>
2026-07-09 22:58:27 -0400

    Exported the db schemas and the barrel export for the types

--- packages/database/mod.ts
+++ packages/database/mod.ts
@@ -1,6 +1,8 @@
 export * from "./drizzle/client.ts"
 export * from "./honker/client.ts"
 
+export * from "./schemas/index.ts"
+
 export * from "./queues/temp.queue.ts"
 export * from "./queues/ingestionDiscovery.queue.ts"
 export * from "./queues/fileModified.queue.ts"
@@ -15,4 +17,4 @@ export * from "./scripts/dbml.ts"
 export * from "./scripts/migrate.ts"
 export * from "./scripts/seed.ts"
 
-export * from "./shared/types/queue.type.ts"
+export * from "./shared/types/index.ts"
