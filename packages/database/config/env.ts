30f41654cbda005eabacb1117524a222110cd76c
Alexis Montes <montes.alexis25@gmail.com>
2026-07-09 22:55:40 -0400

    reverted back to external schema instead using non aliased path

--- packages/database/config/env.ts
+++ packages/database/config/env.ts
@@ -1,12 +1,3 @@
-import { z } from "zod";
-
-// NOTE: declared this schema locally as there is a compatibility issue between folder aliasas and drizzle-kit
-const dbSchema = z.object({
-  CONFIG_DIRECTORY: z.string().default("/app/data/config"),
-  HONKER_LIB_PATH: z.string().default("/honker/libhonker_ext.so"),
-  LOG_LEVEL: z.string().default("info"),
-  PAGE_SIZE: z.number().default(20),
-  DB_FILE_NAME: z.string().default("database.sqlite"),
-});
+import {dbSchema} from "../shared/schemas/db.schema"
 
 export const env = dbSchema.parse(process.env);
