0386f80dc6f3c58195effd9a6efea72019439d83
Alexis Montes <montes.alexis25@gmail.com>
2026-07-09 22:58:05 -0400

    Fix inconsistent formatting

--- packages/database/schemas/mapping/comicSeriesGroupsAggregate.table.ts
+++ packages/database/schemas/mapping/comicSeriesGroupsAggregate.table.ts
@@ -4,19 +4,16 @@ import { sql } from "drizzle-orm/sql";
 import { comicSeriesTable } from "../tables/comicSeries.table.ts";
 import { comicSeriesGroupsTable } from "../tables/comicSeriesGroups.table.ts";
 
-export const comicSeriesGroupsAggregateTable = snakeCase.table(
-  "comic_series_series_groups",
-  {
-    id: int().primaryKey({ autoIncrement: true }),
-    comicSeriesId: int().notNull().references(() => comicSeriesTable.id, {
-      onDelete: "cascade",
-    }),
-    comicSeriesGroupId: int().notNull().references(
-      () => comicSeriesGroupsTable.id,
-      { onDelete: "cascade" },
-    ),
-    position: int().notNull(),
-    createdAt: text().notNull().default(sql`CURRENT_TIMESTAMP`),
-    updatedAt: text().notNull().default(sql`CURRENT_TIMESTAMP`),
-  },
-);
+export const comicSeriesGroupsAggregateTable = snakeCase.table("comic_series_series_groups", {
+  id: int().primaryKey({ autoIncrement: true }),
+  comicSeriesId: int().notNull().references(() => comicSeriesTable.id, {
+    onDelete: "cascade",
+  }),
+  comicSeriesGroupId: int().notNull().references(
+    () => comicSeriesGroupsTable.id,
+    { onDelete: "cascade" },
+  ),
+  position: int().notNull(),
+  createdAt: text().notNull().default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: text().notNull().default(sql`CURRENT_TIMESTAMP`),
+});
