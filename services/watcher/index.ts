8fd4e109e4c538e9e4604e35bedf44e07081d2c3
Alexis Montes <montes.alexis25@gmail.com>
2026-07-11 03:08:07 -0400

    started adding the core watcher logic to the index file

--- services/watcher/index.ts
+++ services/watcher/index.ts
@@ -1,3 +1,18 @@
+
+import { getAllComicLibraries, type ComicLibrary } from "kitsune-komix-database"
+
+import { env } from "./config/env";
 import { WatchManager } from "./watcher/manager";
 
 const workerWatcher = new WatchManager();
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+while (true) {
+  const libraries: ComicLibrary[] = await getAllComicLibraries();
+  const paths: string[] = libraries.map(l => l.path);
+
+  await workerWatcher.syncDirectories(paths);
+  
+  await sleep(env.LIBRARY_SCAN_INTERVAL);
+}
