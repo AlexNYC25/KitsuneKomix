ea48477f3a9c8f4a92d638e07a981273f0215ef9
Alexis Montes <montes.alexis25@gmail.com>
2026-07-13 21:26:46 -0400

    added additional queue names

--- packages/database/config/queues.ts
+++ packages/database/config/queues.ts
@@ -1,7 +1,15 @@
 
 export const QueueNames = {
+  TEMP: "temp",
   INGESTION_DISCOVERY: "ingestion_discovery",
   FILE_MODIFIED: "file_modified",
   FILE_REMOVED: "file_removed",
-  TEMP: "temp"
+  SERIES_DISCOVERY: "series_discovery",
+  SERIES_CREATION: "series_creation",
+  BOOK_SERIES_MAPPING: "book_series_mapping",
+  COMICINFO_EXTRACTION: "comicinfo_extraction",
+  COMICINFO_METADATA_CREATION: "comicinfo_metadata_creation",
+  COMIC_METADATA_AGGREGATION: "comic_metadata_aggregation",
+  PROCESS_COMIC_PAGES: "process_comic_pages",
+  GENERATE_COMIC_THUMBNAILS: "generate_comic_thumbnails"
 }
