998a7e8f4c7e0b19accb504d33396a3d55187b83
Alexis Montes <montes.alexis25@gmail.com>
2026-07-13 21:04:43 -0400

    updated the queue finder to dynamically return various queues using the new getQueue function

--- packages/database/queues/getQueue.ts
+++ packages/database/queues/getQueue.ts
@@ -4,13 +4,15 @@ import { env } from "../config/env.ts"
 import { QueueNames } from "../config/queues.ts"
 import { generateSqlFilePath } from "../utilities/db-file.ts"
 
-export const getIngestionDiscoveryQueue = async () => {
+export const getQueue = async (queueName: keyof typeof QueueNames) => {
   const sqlitePath: string = await generateSqlFilePath(env.CONFIG_DIRECTORY);
   const honkerPath: string = env.HONKER_LIB_PATH
 
   const db: Database = open(sqlitePath, honkerPath);
+ 
+  const mappedQueueName = QueueNames[queueName]
 
-  const q: Queue = db.queue(QueueNames.INGESTION_DISCOVERY);
+  const q: Queue = db.queue(mappedQueueName);
 
   return q;
 }
