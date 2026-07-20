38e68aa77770a4380319acf7ff4690315d1f6575
Alexis Montes <montes.alexis25@gmail.com>
2026-07-19 23:22:57 -0400

    Added initial year parsing as the first example of token consumption

--- services/worker/utilities/comicNameParser.ts
+++ services/worker/utilities/comicNameParser.ts
@@ -2,7 +2,7 @@
 const tokenPatterns = [
   {
     type: "YEAR",
-    regex: /^\((19|20)\d{2}\)/
+    regex: /\((19|20)\d{2}\)/
   },
   {
     type: "VOLUME",
@@ -22,8 +22,50 @@ const tokenPatterns = [
   },
 ]
 
-export const consumeYear = (fileName: string) => {
+type comicNameParserResult = {
+  seriesName: string | undefined,
+  issue: string | undefined,
+  volume: string | undefined,
+  count: number | undefined,
+  year: number | undefined,
+  format: string | undefined,
+  tags: string[]
+}
 
+export const consumeYear = (fileName: string) => {
+  const yearTokenPattern = tokenPatterns.find(i => (i.type === "YEAR"))
+
+  if (yearTokenPattern == undefined) {
+    return {
+      year: undefined,
+      updatedString: fileName
+    }
+  }
+
+  const matches: RegExpMatchArray | null = fileName.match(yearTokenPattern.regex)
+  
+
+  if (matches == null) {
+    return {
+      year: undefined,
+      updatedString: fileName
+    }
+  }
+
+  for (const match of matches) {
+    const rawMatchYear = match.replace("(", "").replace(")", "")
+
+    return {
+      year: parseInt(rawMatchYear),
+      updatedString: fileName.replace(match.toString(), "")
+    }
+
+  }
+
+  return {
+    year: undefined,
+    updatedString: fileName
+  }
 }
 
 export const consumeIssueCount = (fileName: string) => {
@@ -48,4 +90,16 @@ export const consumeIssue = (fileName: string) => {
 
 export const tokenizeComicFileName = (fileName: string) => {
 
+}
+
+export const parseComicNameForDetails = (fileName: string): comicNameParserResult | any => {
+  
+  const yearInfo = consumeYear(fileName)
+  fileName = yearInfo.updatedString
+
+
+  return {
+    year: yearInfo?.year || undefined
+  } as comicNameParserResult
+  
 }
