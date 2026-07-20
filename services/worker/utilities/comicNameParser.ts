1aa82c6474ef643b79d6a3d20507b17ad0312102
Alexis Montes <montes.alexis25@gmail.com>
2026-07-19 23:43:53 -0400

    Added new issue count function parser

--- services/worker/utilities/comicNameParser.ts
+++ services/worker/utilities/comicNameParser.ts
@@ -5,6 +5,10 @@ const tokenPatterns = [
     regex: /\((19|20)\d{2}\)/
   },
   {
+    type: "ISSUE_COUNT",
+    regex: /\((of|Of)\s\d{2}\)/
+  },
+  {
     type: "VOLUME",
     regex: /^v\d+/i
   },
@@ -43,7 +47,6 @@ export const consumeYear = (fileName: string) => {
   }
 
   const matches: RegExpMatchArray | null = fileName.match(yearTokenPattern.regex)
-  
 
   if (matches == null) {
     return {
@@ -69,7 +72,38 @@ export const consumeYear = (fileName: string) => {
 }
 
 export const consumeIssueCount = (fileName: string) => {
+  const issueCountPattern = tokenPatterns.find(i => i.type == "ISSUE_COUNT")
 
+  if (issueCountPattern == undefined) {
+    return {
+      totalIssueCount: undefined,
+      updatedString: fileName
+    }
+  }
+
+  const matches: RegExpMatchArray | null = fileName.match(issueCountPattern.regex)
+
+  if (matches == null ) {
+    return {
+      totalIssueCount: undefined,
+      updatedString: fileName
+    }
+  }
+
+  for (const match of matches) {
+    const rawMatchCount = match.replace("(", "").replace(")", "").replace("of", "").replace("Of", "").trim()
+
+    return {
+      totalIssueCount: parseInt(rawMatchCount),
+      updatedString: fileName.replace(match.toString(), "")
+    }
+
+  }
+
+  return {
+    totalIssueCount: undefined,
+    updatedString: fileName
+  }
 }
 
 export const consumeTags = (fileName: string) => {
@@ -97,9 +131,13 @@ export const parseComicNameForDetails = (fileName: string): comicNameParserResul
   const yearInfo = consumeYear(fileName)
   fileName = yearInfo.updatedString
 
+  const issueCountInfo = consumeIssueCount(fileName)
+  fileName = issueCountInfo.updatedString
+
 
   return {
-    year: yearInfo?.year || undefined
+    year: yearInfo?.year || undefined,
+    count: issueCountInfo?.totalIssueCount || undefined,
   } as comicNameParserResult
   
 }
