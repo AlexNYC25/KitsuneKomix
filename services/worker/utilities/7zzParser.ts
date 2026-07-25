c53170851e2396c453e0a22cba4db39a9fdae1c3
Alexis Montes <montes.alexis25@gmail.com>
2026-07-25 19:10:24 -0400

    added a parser to convert the expected raw output to an object based format

---
+++ services/worker/utilities/7zzParser.ts
@@ -0,0 +1,26 @@
+import type { List7zzFileOutput } from "../shared/types/utilities.types"
+
+const entryRegex = /^(\d{4}-\d{2}-\d{2})\s+(\d{2}:\d{2}:\d{2})\s+(\S{5})\s+(\d+)\s+(\d+)\s+(.+)$/
+
+/**
+ * Takes the raw output of the 7zz exec, and parses it to an object format
+ * @param output the string output from the list 7zz wrapper function
+ * @returns an array of List7zzFileOutput objects
+ */
+export const parseListOutput = (output: string): List7zzFileOutput[] => {
+  const lines: string[] = output.trim().split('\n')
+
+  const parsedLines: List7zzFileOutput[] = []
+
+  for (const line of lines) {
+    const match: RegExpMatchArray | null = line.match(entryRegex)
+
+    if (!match) continue
+
+    const [, date, time, attr, size, compressed, name] = match
+
+    parsedLines.push({ date, time, attr, size, compressed, name } as List7zzFileOutput)
+  }
+
+  return parsedLines
+}
