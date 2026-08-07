88e0ac60bd56d9d16924f9fe8efa9d02ad498860
Alexis Montes <montes.alexis25@gmail.com>
2026-08-06 22:03:22 -0400

    added the start of the comic series model functions including the function to complete the association between a comicbook and comic series

---
+++ packages/database/models/comicSeries.model.ts
@@ -0,0 +1,51 @@
+import { eq, sql } from "drizzle-orm";
+
+import { getClient } from "../drizzle/client.ts";
+import { dbLogger } from "../loggers/index.ts";
+import { env } from "../config/env.ts"
+
+import {
+  comicSeriesBooksTable,
+} from "../schemas/index.ts";
+
+import type {
+  ComicBook,
+  NewComicBook,
+} from "../shared/types/index.ts"
+
+
+/**
+ * Adds a comic book to a series by creating a relationship
+ *
+ * used by the internal worker
+ *
+ * @param seriesId The ID of the series
+ * @param comicBookId The ID of the comic book
+ * @returns void
+ */
+export const addComicBookToSeries = async (
+  seriesId: number,
+  comicBookId: number,
+): Promise<boolean> => {
+  const db = await getClient();
+
+  if (!db) {
+    throw new Error("Database is not initialized.");
+  }
+
+  try {
+    const result: { id: number }[] = await db
+      .insert(comicSeriesBooksTable)
+      .values({
+        comicSeriesId: seriesId,
+        comicBookId: comicBookId,
+      })
+      .onConflictDoNothing()
+      .returning({ id: comicSeriesBooksTable.id });
+
+    return result.length > 0;
+  } catch (error) {
+    dbLogger.error("Error adding comic book to series:" + error);
+    throw error;
+  }
+};
