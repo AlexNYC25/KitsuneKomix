96836428873040ad05bba7f76bf4544a2165db79
Alexis Montes <montes.alexis25@gmail.com>
2026-08-16 22:45:28 -0400

    Added a logic note

--- services/worker/workers/comicBookSeriesMapping.worker.ts
+++ services/worker/workers/comicBookSeriesMapping.worker.ts
@@ -50,7 +50,15 @@ export class ComicBookSeriesMappingWorker {
     const currentPayload: IngestionToComicSeriesMappingPayload = job.payload as IngestionToComicSeriesMappingPayload
 
     try {
-      const comicBookId: number = currentPayload.comicBookId
+      const comicSeriesId: number | null = null // TODO: Implement logic to find the series ID based on the comic book details
+
+      if (comicSeriesId) {
+        await addComicBookToSeries(comicSeriesId, currentPayload.comicBookId)
+      } else {
+        // TODO: Create a new series and add the comic book to it
+        workerLogger.error("Could not find a matching series for the comic book")
+      }
+      
 
     } catch {
       workerLogger.error("There was an error parsing and inserting the initial comic book record")
